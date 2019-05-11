18875f40aae14d2ed093df95e8fbb839e7067aa2
user <user@localhost>
2019-05-11 11:31:29 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -2,7 +2,7 @@ class WebResource
   module HTTP
 
     # Hosts with OPTIONS/POST/PUT capability
-    POSThosts = /(\.(edu|gov)|(anvato|api\.(brightcove|twitter)|(android.*|clients?[0-9]?|drive|groups|images|mail|www)\.google|mirrors.lolinent|reddit|soundcloud|youtube|talk.zerohedge|zillow)\.(com|net))$/
+    POSThosts = /(\.(edu|gov)|(anvato|api\.(brightcove|twitter)|(android.*|clients?[0-9]?|drive|groups|images|mail|www)\.google|android.googleapis|mirrors.lolinent|reddit|soundcloud|youtube|talk.zerohedge|zillow)\.(com|net))$/
 
     # upstream user-interface by default
     UI = {
