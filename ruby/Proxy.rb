306bdecb2b020fcf8d4938a2aa16c854f3831792
user <user@localhost>
2019-05-24 01:54:34 +0000

    p

--- ruby/Proxy.rb
+++ ruby/Proxy.rb
@@ -238,7 +238,7 @@ class WebResource
       if env.has_key? 'HTTP_TYPE'
         case env['HTTP_TYPE']
         when /drop/
-          if env['REQUEST_URI'].match?(/track/) && host.match?(TrackHost)
+          if ((host.match? /track/) || (env['REQUEST_URI'].match? /track/)) && (host.match? TrackHost)
             fetch
           elsif qs == '?allow'
             puts "ALLOW #{uri}"
