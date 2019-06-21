22b44d881849aa7f063f7334811906956e39a7fb
poka <pokatet@gmail.com>
2019-06-21 00:45:45 +0000

    p

--- ruby/Formats.rb
+++ ruby/Formats.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 class WebResource
-  RDFformats = /^(application|text)\/(atom|html|rss|turtle|xml)/
+  RDFformats = /^(application|text)\/(atom|html|json|rss|turtle|xml)/
+
   module Calendar
     class Format < RDF::Format
       content_type 'text/calendar', :extension => :ics
@@ -327,6 +328,35 @@ class WebResource
               end
             }
           end}
+      end
+    end
+  end
+  module GIF
+    class Format < RDF::Format
+      content_type 'image/gif', :extension => :gif
+      content_encoding 'utf-8'
+      reader { WebResource::GIF::Reader }
+    end
+
+    class Reader < RDF::Reader
+      include URIs
+      format Format
+
+      def initialize(input = $stdin, options = {}, &block)
+        @subject = (options[:base_uri] || '#image').R 
+        @img = Exif::Data.new(input.respond_to?(:read) ? input.read : input) rescue nil #puts("EXIF read failed on #{@subject}")
+        if block_given?
+          case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+          end
+        end
+        nil
+      end
+
+      def each_triple &block; each_statement{|s| block.call *s.to_triple} end
+
+      def each_statement &fn
       end
     end
   end
