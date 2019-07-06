ec1ae578ce7b8738d749c7e480b06cee42870c16
user <user@localhost>
2019-07-06 04:56:19 +0000

    p

---
+++ ruby/Formats/Video.rb
@@ -0,0 +1,30 @@
+module Webize
+  module MP4
+    class Format < RDF::Format
+      content_type 'video/mp4', :extension => :mp4
+      reader { Reader }
+    end
+
+    class Reader < RDF::Reader
+      include WebResource::URIs
+      format Format
+
+      def initialize(input = $stdin, options = {}, &block)
+        @subject = (options[:base_uri] || '#mp3').R 
+        @img = Exif::Data.new(input.respond_to?(:read) ? input.read : input)
+        if block_given?
+          case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+          end
+        end
+        nil
+      end
+
+      def each_triple &block; each_statement{|s| block.call *s.to_triple} end
+
+      def each_statement &fn
+      end
+    end
+  end
+end
