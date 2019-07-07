5155a7a28b86463993ae8a3950b3a7a44b49f0d9
user <user@localhost>
2019-07-07 21:14:29 +0000

    p

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,20 +1,4 @@
-%w{
-brotli
-cgi csv
-date digest/sha2 dimensions
-exif
-fileutils
-httparty
-icalendar
-json
-linkeddata
-mail
-nokogiri
-open-uri
-pathname
-rack rdf redcarpet
-shellwords
-}.map{|r|require r}
+%w{brotli cgi csv date digest/sha2 dimensions exif fileutils httparty icalendar json linkeddata mail nokogiri open-uri pathname rack rdf redcarpet shellwords}.map{|r|require r}
 class RDF::URI
   def R; WebResource.new to_s end
 end
@@ -28,27 +12,28 @@ class String
 end
 class WebResource < RDF::URI
   def R; self end
+  alias_method :uri, :to_s
   module URIs
+    CacheDir = '../.cache/web/'
+    ConfDir = Pathname.new(__dir__).join('../config').relative_path_from Pathname.new Dir.pwd
+
     W3       = 'http://www.w3.org/'
     DC       = 'http://purl.org/dc/terms/'
     SIOC     = 'http://rdfs.org/sioc/ns#'
-    Abstract = DC   + 'abstract'
+    Abstract = DC + 'abstract'
     Content  = SIOC + 'content'
     Creator  = SIOC + 'has_creator'
-    Date     = DC   + 'date'
+    Date     = DC + 'date'
     Image    = DC + 'Image'
     Link     = DC + 'link'
     Post     = SIOC + 'Post'
     Schema   = 'http://schema.org/'
-    Title    = DC   + 'title'
+    Title    = DC + 'title'
     To       = SIOC + 'addressed_to'
     Type     = W3 + '1999/02/22-rdf-syntax-ns#type'
     Video    = DC + 'Video'
-    CacheDir = '../.cache/web/'
-    ConfDir = Pathname.new(__dir__).join('../config').relative_path_from Pathname.new Dir.pwd
   end
   include URIs
-  alias_method :uri, :to_s
 end
 # require library and site config
 %w(POSIX HTTP).map{|_| require_relative _}
