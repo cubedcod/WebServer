3a426bfa5bce3e357435d1a6a3c5c87ed980d826
poka <pokatet@gmail.com>
2019-07-09 16:47:49 +0000

    p

--- ruby/Formats/Video.rb
+++ ruby/Formats/Video.rb
@@ -26,4 +26,31 @@ module Webize
       end
     end
   end
+  module WebM
+    class Format < RDF::Format
+      content_type 'video/webm', :extension => :webm
+      reader { Reader }
+    end
+
+    class Reader < RDF::Reader
+      include WebResource::URIs
+      format Format
+
+      def initialize(input = $stdin, options = {}, &block)
+        @subject = (options[:base_uri] || '#mp3').R 
+        if block_given?
+          case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+          end
+        end
+        nil
+      end
+
+      def each_triple &block; each_statement{|s| block.call *s.to_triple} end
+
+      def each_statement &fn
+      end
+    end
+  end
 end
