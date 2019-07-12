06c6bfeb5974e4a310d82afb698508018621c251
poka <pokatet@gmail.com>
2019-07-12 04:13:16 +0000

    p

--- ruby/Formats/HTML.rb
+++ ruby/Formats/HTML.rb
@@ -360,17 +360,16 @@ class WebResource
                                  {_: :link, rel: type, href: CGI.escapeHTML(uri.to_s)}}
                             ].map{|e|['  ',e,"\n"]}}, "\n\n",
                         {_: :body,
-                         c: ["\n", link[:up, '&#9650;'],
+                         c: ["\n", link[:up, '&#9650;'], link[:prev, '&#9664;'], link[:next, '&#9654;'],
                              {_: :a, id: :tabular, style: tabular ? 'color: #fff' : 'color: #555',
                               href: HTTP.qs(tabular ? q.reject{|k,v|k=='view'} : q.merge({'view' => 'table', 'sort' => 'date'})), c: '↨'},
                              {_: :a, id: :shrink, style: shrunken ? 'color: #fff' : 'color: #555',
                               href: HTTP.qs(shrunken ? q.reject{|k,v|k=='head'} : q.merge({'head' => ''})), c: shrunken ? '&#9661;' : '&#9651;'},
-                             {_: :a, id: :ui, style: 'color: #555', href: HTTP.qs(q.merge({'ui' => ''})), c: '⚗'},
-                             link[:prev, '&#9664;'], link[:next, '&#9654;'],
                              unless local?
-                               {class: :toolbox,
+                               [{_: :a, id: :ui, style: 'color: #555', href: HTTP.qs(q.merge({'ui' => ''})), c: '⚗'},
+                                {class: :toolbox,
                                 c: {_: :a, id: :subscribe,
-                                    href: '/' + (subbed ? 'un' : '') + 'subscribe' + HTTP.qs({u: 'https://' + host + (@r['REQUEST_URI'] || path)}), class: subbed ? :on : :off, c: 'subscribe' + (subbed ? 'd' : '')}}
+                                    href: '/' + (subbed ? 'un' : '') + 'subscribe' + HTTP.qs({u: 'https://' + host + (@r['REQUEST_URI'] || path)}), class: subbed ? :on : :off, c: 'subscribe' + (subbed ? 'd' : '')}}]
                              end,
                              if graph.empty?
                                HTML.keyval (Webize::HTML.webizeHash @r), @r # 404
