aa126690ea4c381cdd3cca8ead4e88aaaa699a14
author <pokatet@gmail.com>
2019-07-22 11:16:11 +0000

    p

--- ruby/Formats/HTML.rb
+++ ruby/Formats/HTML.rb
@@ -560,9 +560,14 @@ class WebResource
 
     def self.tree t, env, name=nil
       url = t[:RDF]['uri'] if t[:RDF]
-      css = {style: "background-color: #{'#%06x' % rand(16777216)}"} if name && t.keys.size > 1
+      if name && t.keys.size > 1
+        color = '#%06x' % rand(16777216)
+        scale = rand(7) + 1
+        position = scale * rand(960) / 960.0
+        css = {style: "border: .08em solid #{color}; background: repeating-linear-gradient(#{rand 360}deg, #000, #000 #{position}em, #{color} #{position}em, #{color} #{scale}em)"}
+      end
       {class: :tree,
-       c: [({_: (url ? :a : :span), c: (CGI.escapeHTML name.to_s[0..85])}.update(url ? {href: url} : {}) if name),
+       c: [({_: :a, href: url, c: CGI.escapeHTML(name.to_s[0..85])} if name && url),
            t.map{|_name, _t|
              _name == :RDF ? (value nil, _t, env) : (tree _t, env, _name)}]}.update(css ? css : {})
     end
