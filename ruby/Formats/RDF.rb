27f1bff94d97567814103686f9a60ed2d79a2f54
poka <pokatet@gmail.com>
2019-07-24 02:05:14 +0000

    p

--- ruby/Formats/RDF.rb
+++ ruby/Formats/RDF.rb
@@ -40,3 +40,41 @@ class WebResource
   end
 
 end
+module Webize
+  module URIlist
+    class Format < RDF::Format
+      content_type 'text/uri-list',
+                   extension: :u
+      content_encoding 'utf-8'
+
+      reader { Reader }
+    end
+
+    class Reader < RDF::Reader
+      include WebResource::URIs
+      format Format
+
+      def initialize(input = $stdin, options = {}, &block)
+        @doc = input.respond_to?(:read) ? input.read : input
+        if block_given?
+          case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+          end
+        end
+        nil
+      end
+
+      def each_triple &block; each_statement{|s| block.call *s.to_triple} end
+
+      def each_statement &fn
+        @doc.lines.map{|line|
+          line = line.chomp
+          resource = line.R
+          fn.call RDF::Statement.new(resource, Type.R, (W3 + '2000/01/rdf-schema#Resource').R)
+          fn.call RDF::Statement.new(resource, Title.R, RDF::Literal(line))
+        }
+      end
+    end
+  end
+end
