11c825e27605ae7dcb468271566c19e25a0876b6
poka <carmen@replic.net>
2019-08-18 00:21:52 +0000

    p

--- config/alternatives/imasdk.googleapis.com/js/sdkloader/ima3.js
+++ config/alternatives/imasdk.googleapis.com/js/sdkloader/ima3.js
@@ -1,4 +1,16 @@
 google = {ima: {ImaSdkSettings: {VpaidMode: {ENABLED: 1}},
 		settings: {setVpaidMode: function(bool){
 		    console.log('Welcome to IMA');
-		}}}};
+		}},
+	       	AdDisplayContainer: class {
+		    constructor(a,b,c,d) {
+			console.log('..new display container');
+		    };
+		},
+		AdsLoader: class {
+		    constructor(a) {
+			console.log('..new ad loader');
+		    };
+		}
+	       }};
+
