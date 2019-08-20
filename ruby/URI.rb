284dd7f8c9ac29c3a5512b9ed871df8e81f239cd
poka <carmen@replic.net>
2019-08-20 16:27:25 +0000

    p

--- ruby/URI.rb
+++ ruby/URI.rb
@@ -1,15 +1,18 @@
 # coding: utf-8
-require 'pathname'
-require 'linkeddata'
+%w(digest/sha2 fileutils linkeddata pathname shellwords).map{|_| require _}
+
 class RDF::URI
   def R env=nil; env ? WebResource.new(to_s).env(env) : WebResource.new(to_s) end
 end
+
 class RDF::Node
   def R env=nil; env ? WebResource.new(to_s).env(env) : WebResource.new(to_s) end
 end
+
 class String
   def R env=nil; env ? WebResource.new(self).env(env) : WebResource.new(self) end
 end
+
 class WebResource < RDF::URI
   def R; self end
   alias_method :uri, :to_s
@@ -410,10 +413,68 @@ class WebResource < RDF::URI
     end
 
   end
+
   include URIs
+
   module HTML
     include URIs
     Markup = {}
   end
+
   include HTML
+
+  module POSIX
+    include URIs
+    def basename; File.basename ( path || '/' ) end                     # BASENAME(1)
+    def children; node.children.delete_if{|f|f.basename.to_s.index('.')==0}.map &:toWebResource end
+    def dir; dirname.R if path end                                      # DIRNAME(1)
+    def dirname; File.dirname path if path end                          # DIRNAME(1)
+    def du; `du -s #{shellPath}| cut -f 1`.chomp.to_i end               # DU(1)
+    def exist?; node.exist? end
+    def ext; File.extname( path || '' )[1..-1] || '' end
+    def file?; node.file? end
+    def find p; `find #{shellPath} -iname #{Shellwords.escape p}`.lines.map{|p|POSIX.path p} end # FIND(1)
+    def glob; Pathname.glob(relPath).map{|p|p.toWebResource env} end    # GLOB(7)
+    def grep # URI -> file(s)                                           # GREP(1)
+      args = POSIX.splitArgs env[:query]['q']
+      case args.size
+      when 0
+        return []
+      when 2 # two unordered terms
+        cmd = "grep -rilZ #{Shellwords.escape args[0]} #{shellPath} | xargs -0 grep -il #{Shellwords.escape args[1]}"
+      when 3 # three unordered terms
+        cmd = "grep -rilZ #{Shellwords.escape args[0]} #{shellPath} | xargs -0 grep -ilZ #{Shellwords.escape args[1]} | xargs -0 grep -il #{Shellwords.escape args[2]}"
+      when 4 # four unordered terms
+        cmd = "grep -rilZ #{Shellwords.escape args[0]} #{shellPath} | xargs -0 grep -ilZ #{Shellwords.escape args[1]} | xargs -0 grep -ilZ #{Shellwords.escape args[2]} | xargs -0 grep -il #{Shellwords.escape args[3]}"
+      else # N ordered terms
+        pattern = args.join '.*'
+        cmd = "grep -ril #{Shellwords.escape pattern} #{shellPath}"
+      end
+      `#{cmd} | head -n 1024`.lines.map{|path|POSIX.path path}
+    end
+    def ln   n; FileUtils.ln   node.expand_path, n.node.expand_path end # LN(1)
+    def ln_s n; FileUtils.ln_s node.expand_path, n.node.expand_path end # LN(1)
+    def link n; n.dir.mkdir; send :ln, n unless n.exist? end            # LN(1)
+    def mkdir; FileUtils.mkdir_p relPath unless exist?; self end        # MKDIR(1)
+    def node; @node ||= (Pathname.new relPath) end
+    def parts; path ? path.split('/').-(['']) : [] end
+    def relPath; URI.unescape(['/','','.',nil].member?(path) ? '.' : (path[0]=='/' ? path[1..-1] : path)) end
+    def self.path p; ('/' + p.to_s.chomp.gsub(' ','%20').gsub('#','%23')).R end
+    def self.splitArgs args; args.shellsplit rescue args.split /\W/ end
+    def shellPath; Shellwords.escape relPath.force_encoding 'UTF-8' end
+    def touch; dir.mkdir; FileUtils.touch relPath end                   # TOUCH(1)
+    def write o; dir.mkdir; File.open(relPath,'w'){|f|f << o}; self end
+  end
+
+  include POSIX
+
+end
+class Pathname
+  def toWebResource env = nil
+    if env
+     (WebResource::POSIX.path self).env env
+    else
+      WebResource::POSIX.path self
+    end
+  end
 end
--- ruby/index.rb
+++ ruby/index.rb
@@ -1,5 +1,9 @@
-%w(URI POSIX).map{|lib|require_relative lib}
-%w(Gopher HTTP NNTP).map{|proto|require_relative 'Protocols/' + proto}
-%w(Audio Calendar CSS Feed HTML Image JS LaTeX Markdown Message PDF RDF SQL Text Video Web YAML).map{|format|require_relative 'Formats/' + format}
+require_relative 'URI'
+
+%w(Gopher HTTP NNTP).map{|proto|
+  require_relative 'Protocols/' + proto}
+
+%w(Audio Calendar CSS Feed HTML Image JS LaTeX Markdown Message PDF RDF SQL Text Video Web YAML).map{|format|
+  require_relative 'Formats/' + format}
 
 require_relative '../config/site.rb'
