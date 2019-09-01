bd4a8deacfa092f8c4231e61b822922d368c4547
poka <carmen@replic.net>
2019-09-01 14:35:20 +0000

    p

--- index.rb
+++ index.rb
@@ -1,7 +1,7 @@
 
 %w(URI Archive Audio Calendar CSS Feed HTML Image JS LaTeX Markdown Message PDF RDF SQL Text Video Web YAML).map{|format|
   require_relative 'Formats/' + format}
-%w(Gopher HTTP NNTP).map{|proto|
-  require_relative 'Protocols/' + proto}
-
-require_relative 'config/site.rb'
+%w(Gopher HTTP NNTP).map{|protocol|
+  require_relative 'Protocols/' + protocol}
+%w(meta site).map{|config|
+  require_relative 'config/' + config}
