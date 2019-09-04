fa5f98ba341f786015dd3617c5bc356c8f9ea9df
poka <carmen@replic.net>
2019-09-04 22:29:01 +0000

    p

--- Protocols/HTTP.rb
+++ Protocols/HTTP.rb
@@ -111,9 +111,9 @@ class WebResource
                 end) + ';1'
 
         puts "\e[7m" + (env['REQUEST_METHOD'] == 'GET' ? '' : env['REQUEST_METHOD']) +
-             "\e[" + color + "m"  + status.to_s + (env['HTTP_REFERER'] ? (' ' + (env['HTTP_REFERER'].R.host || '').sub(/^www\./,'').sub(/\.com$/,'') + "\e[0m -> ") : ' ') +
+             "\e[" + color + "m"  + status.to_s + (env['HTTP_REFERER'] ? (' ' + (env['HTTP_REFERER'].R.host || '').sub(/^www\./,'').sub(/\.com$/,'') + "\e[0m→") : ' ') +
              "\e[" + color + ";7mhttps://" + env['SERVER_NAME'] + "\e[0m\e[" + color + "m" + env['REQUEST_PATH'] + (env['QUERY_STRING'] && !env['QUERY_STRING'].empty? && ('?'+env['QUERY_STRING']) || '') +
-             "\e[0m " + (head['Location'] ? (" ↝ " + head['Location']) : '') + ' ' +
+             "\e[0m " + (head['Location'] ? ("➡️" + head['Location']) : '') + ' ' +
              (head['Content-Type'] == 'text/turtle; charset=utf-8' ? '🐢' : (head['Content-Type']||''))
 
         [status, head, body]} # response
