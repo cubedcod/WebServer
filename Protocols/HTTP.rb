1783da7df957c420176eaa669a76410d336bac3d
poka <carmen@replic.net>
2019-09-05 21:37:38 +0000

    p

--- Protocols/HTTP.rb
+++ Protocols/HTTP.rb
@@ -78,7 +78,7 @@ class WebResource
     end
 
     def cached
-      %w(apk css gif jpg js pdf png mp3 mp4 opus svg webm webp).member?(ext.downcase) &&
+      %w(apk css gif jpeg jpg js pdf png mp3 mp4 opus svg webm webp).member?(ext.downcase) &&
       file? &&
       self
     end
@@ -660,7 +660,7 @@ x-forwarded-for}.member?(key.downcase)
       HostPOST[host] = lambda
     end
 
-    def POSTresource
+    def POSTrequest
       if handler = HostPOST[host] # host handler
         handler[self]
       else
--- config/site.rb
+++ config/site.rb
@@ -62,7 +62,7 @@ class WebResource
   end
   module HTTP
     CDN = /amazon|azure|cloud(flare|front|inary)|digitalocean|fa(cebook|stly)|heroku|netdna|ra(ckcdn|wgit)|stackpath|usercontent/
-    CDNsubdomain = /\.(s3\.amazonaws|storage\.googleapis)\.com$/
+    CDNsubdomain = /(s3\.amazonaws|storage\.googleapis)\.com$/
     DesktopUA = ['Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3896.4 Safari/537.36',
                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3895.3 Safari/537.36',
                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
