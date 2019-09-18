e985dbcc87408545714c4dd02af6946f05f71da0
poka <carmen@replic.net>
2019-09-18 07:59:19 +0000

    p

--- INSTALL.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-name=WebServer
-src=~/src/$name
-[ -e $src ] || git clone https://gitlab.com/ix/$name $src
-$src/bin/INSTALL
--- Protocols/HTTP.rb
+++ Protocols/HTTP.rb
@@ -275,7 +275,7 @@ class WebResource
           env[:repository] ||= RDF::Repository.new                       # RDF storage
           RDF::Reader.for(content_type: format).yield_self{|rdr|         # RDF reader
             rdr.new(body, {base_uri: self, noRDF: env[:noRDF]}){|rdf|    # read RDF
-              env[:repository] << rdf } if rdr; puts "NO_READER #{format} #{uri}" unless rdr }
+              env[:repository] << rdf } if rdr}
           return self if env[:intermediate]                              # just fetch, no response
           index                                                          # index RDF
           %w{Access-Control-Allow-Origin Access-Control-Allow-Credentials Content-Type ETag Set-Cookie}.map{|k|
