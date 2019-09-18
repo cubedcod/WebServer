4e260837fe413a6ecffc9099701b4a5f0d59db06
poka <carmen@replic.net>
2019-09-18 02:41:52 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -54,7 +54,7 @@ new(relic|sletter)|
 o(m(niture|tr)|nboarding|ptanon|utbrain)|
 p(a(idpost|y(ments?|wall))|er(imeter-?x|sonaliz(ation|e))|i(wik|xel(propagate)?)|lacement|op(over|up)|romo(tion)?s?|ubmatic|[vx])|
 quantcast|
-reco(mmend(ation)?s?|rd(event|stats?))|re?t(ar)?ge?t(ing)?|rpc|
+reco(mmend(ation)?s?|rd(event|stats?))|re?t(ar)?ge?t(ing)?|remote[-_]?(control)?|rpc|
 s?s(a(fe[-_]?browsing|ilthru)|ervice[-_]?worker|i(ftscience|gnalr|tenotice)|o(cial|urcepoint)|ponsored|so|tat(istic)?s?|ubscri(ber?|ption)|w.js|ync)|
 t(aboola|(arget|rack)(ers?|ing)|bproxy|ea(lium|ser)|hirdparty|inypass|racing|rend(ing|s))|autotrack|
 u(rchin|serlocation|tm)|
