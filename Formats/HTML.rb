b37dbcb1472a197d8a1e468b7be9bf49e694a718
poka <carmen@replic.net>
2019-09-29 03:06:22 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -261,7 +261,7 @@ class WebResource
                                    c: icon.cache.exist? ? {_: :img, src: icon.uri} : host},
                                   parts.map{|p| [{_: :a, class: :breadcrumb, href: bc += '/' + p, c: p, id: 'r'+Digest::SHA2.hexdigest(rand.to_s)}, '&nbsp; ']},
                                   link[:up, '&#9650;'],
-                                  ({_: :a, id: :tabular, style: 'color: #555', c: '↨',
+                                  ({_: :a, id: :tabular, class: :icon, style: 'color: #555', c: '↨',
                                     href: HTTP.qs((env[:query]||{}).merge({'view' => 'table', 'sort' => 'date'}))} unless env[:query] && env[:query]['view']=='table'),
                                   ({_: :a, id: :UX, class: :icon, style: 'color: #555', c: '⚗️',
                                     href: HTTP.qs((env[:query]||{}).merge({'UX' => 'upstream'}))} unless local?)
