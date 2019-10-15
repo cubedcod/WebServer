a2cd03c6c8ea8410c031fbc2b3f95955331f285d
inspiron <carmen@replic.net>
2019-10-15 12:35:07 +0000

    p

--- Formats/RDF.rb
+++ Formats/RDF.rb
@@ -30,18 +30,18 @@ class WebResource
   end
 
   module POSIX
-
+    GraphExt = /\.(md|ttl|u)$/
     # filesystem metadata -> Graph
     def nodeStat options = {}
       return if basename.index('msg.') == 0 || ext=='ttl'           # hide native graph-storage files
-      subject = (options[:base_uri] || path.sub(/\.(md|ttl)$/,'')).R
+      subject = (options[:base_uri] || path.sub(GraphExt,'')).R
       graph = env[:repository] ||= RDF::Repository.new
       if node.directory?
         subject = subject.path[-1] == '/' ? subject : (subject+'/') # enforce trailing slash on container URIs
         graph << (RDF::Statement.new subject, Type.R, (W3+'ns/ldp#Container').R)
         node.children.map{|n|                                       # point to contained nodes
           name = n.basename.to_s
-          name = n.directory? ? (name + '/') : name.sub(/\.ttl$/, '')
+          name = n.directory? ? (name + '/') : name.sub(GraphExt, '')
           child = subject.join name
           graph << (RDF::Statement.new subject, (W3+'ns/ldp#contains').R, child)
           graph << (RDF::Statement.new child, Title.R, name)
