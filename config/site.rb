2427cffeb9b10c156d4d0883111903d23f5be3e7
inspiron <carmen@replic.net>
2019-10-15 23:40:22 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -66,9 +66,9 @@ wp-rum)
 activeview activity-stream addthis_widget.js admin-ajax.php ads ad_status.js all.js analytics.js annotations_invideo api.js apstag.js arwing atrk.js attribution avatar
  b.gif beacon.js blank.gif bullseye buttons.js bz c.gif cast_sender.js chartbeat.js collect conv config.js count.js counter.js count.json css crx
  download downloads ddljson embed.js embeds.js endscreen.js events experimentstatus
- falco favicon.ico fbevents.js FeedQuery fonts fullHashes:find g.gif id inflowcomponent get_endscreen get_midroll_info gpt.js gtm.js ima3.js in.js
+ falco favicon.ico fbevents.js FeedQuery fonts fullHashes:find g.gif id inflowcomponent get_endscreen get_midroll_info gpt.js gtm.js ima3.js i.js in.js
  jot js json ListAccounts load load.js loader.js log log_event logging_client_events lvz m newtab_ogb newtab_promos outbrain.js
- p p.js page_view pay ping ping.gif ping-centre platform.js pixel pixel.gif ptracking push_service_worker.js
+ p p.js page_view pay ping ping.gif ping-centre platform.js pixel pixel.gif pixelpropagate.js ptracking push_service_worker.js
  qoe quant.js query remote.js remote-login.php rtm rundown
  scheduler.js script.js search seed serviceworker service-worker.js sdk.js service_ajax session sw.js sync
  threatListUpdates:fetch tr track tracker trends uc.js utag.js view w.js widgets.js yql)
@@ -674,6 +674,7 @@ heartbeat iframe_api live_chat manifest.json opensearch playlist results signin 
         yield subject, Creator, author.inner_text
       end
       yield subject, To, self
+      comment.remove
     }
   end
 
