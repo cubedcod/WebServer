6cfb8f7f5faa682a6d1eb10e2353e50cb92c3175
inspiron <carmen@replic.net>
2019-10-15 22:21:38 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -62,7 +62,16 @@ wp-rum)
 ([-.:_\/?&=~]|$)|
 \.(eot|gif\?|otf|ttf|woff2?))xi
 
-    QuietGunk = %w(activeview activity-stream addthis_widget.js admin-ajax.php ads ad_status.js all.js analytics.js annotations_invideo api.js apstag.js arwing atrk.js attribution avatar b.gif beacon.js blank.gif bullseye buttons.js bz c.gif cast_sender.js chartbeat.js collect conv config.js count.js counter.js count.json css crx download downloads ddljson embed.js embeds.js endscreen.js events experimentstatus falco favicon.ico fbevents.js FeedQuery fonts fullHashes:find g.gif id inflowcomponent get_endscreen get_midroll_info gpt.js gtm.js ima3.js in.js js json ListAccounts load load.js loader.js log log_event logging_client_events lvz m newtab_ogb newtab_promos outbrain.js p p.js page_view pay ping ping.gif ping-centre platform.js pixel pixel.gif ptracking push_service_worker.js qoe quant.js query remote.js remote-login.php rtm rundown scheduler.js script.js search seed serviceworker service-worker.js sdk.js service_ajax session sw.js sync threatListUpdates:fetch tr track tracker uc.js utag.js view w.js widgets.js yql)
+    QuietGunk = %w(
+activeview activity-stream addthis_widget.js admin-ajax.php ads ad_status.js all.js analytics.js annotations_invideo api.js apstag.js arwing atrk.js attribution avatar
+ b.gif beacon.js blank.gif bullseye buttons.js bz c.gif cast_sender.js chartbeat.js collect conv config.js count.js counter.js count.json css crx
+ download downloads ddljson embed.js embeds.js endscreen.js events experimentstatus
+ falco favicon.ico fbevents.js FeedQuery fonts fullHashes:find g.gif id inflowcomponent get_endscreen get_midroll_info gpt.js gtm.js ima3.js in.js
+ jot js json ListAccounts load load.js loader.js log log_event logging_client_events lvz m newtab_ogb newtab_promos outbrain.js
+ p p.js page_view pay ping ping.gif ping-centre platform.js pixel pixel.gif ptracking push_service_worker.js
+ qoe quant.js query remote.js remote-login.php rtm rundown
+ scheduler.js script.js search seed serviceworker service-worker.js sdk.js service_ajax session sw.js sync
+ threatListUpdates:fetch tr track tracker trends uc.js utag.js view w.js widgets.js yql)
 
     SiteDir  = (Pathname.new __dir__).relative_path_from Pathname.new Dir.pwd
 
