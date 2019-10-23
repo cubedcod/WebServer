b32c6f2ac866f3ba3b11b7f5e6086026efada7c8
inspiron <carmen@replic.net>
2019-10-23 16:44:35 +0000

    p

--- config/alternatives/imasdk.googleapis.com/js/sdkloader/ima3.js
+++ config/alternatives/imasdk.googleapis.com/js/sdkloader/ima3.js
@@ -1,7 +1,9 @@
 google = {ima: {ImaSdkSettings: {VpaidMode: {ENABLED: 1},
 				 setAutoPlayAdBreaks: function(a){},
-				 setPlayerVersion: function(v){},
+				 setDisableCustomPlaybackForIOS10Plus: function(a){},
+				 setLocale: function(a){},
 				 setPlayerType: function(t){},
+				 setPlayerVersion: function(v){},
 				 setVpaidMode: function(b){}
 				},
 	       	AdDisplayContainer: class {
@@ -24,8 +26,7 @@ google = {ima: {ImaSdkSettings: {VpaidMode: {ENABLED: 1},
 		},
 		AdErrorEvent: {Type: {AD_ERROR: 1}},
 		AdsManagerLoadedEvent: {Type: {ADS_MANAGER_LOADED: 1}},
-		settings: {setDisableCustomPlaybackForIOS10Plus: function(a){},
-			   setLocale: function(a){}},
 		version: 1
 	       }};
 
+google.ima.settings = google.ima.ImaSdkSettings;
