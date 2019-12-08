f94a110ed80508613f6359a0944e1952902a5628
moto <carmen@replic.net>
2019-12-08 20:18:19 +0000

    p

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,6 +5,7 @@ which apk && su -c 'apk add alpine-sdk iptables ip6tables graphicsmagick ruby ru
 
 # Arch Linux https://www.archlinux.org/
 which pacman && su -c 'pacman -S graphicsmagick git base-devel ruby ruby-bundler ruby-rdoc python-pip pkg-config tmux squid make rsync'
+which pacman && su -c '/usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB'
 
 # Debian https://www.debian.org/
 which apt-add-repository && su -c 'apt-get install graphicsmagick git ruby ruby-dev bundler grep file pkg-config libexif-dev libssl-dev libxslt-dev libzip-dev tmux squid make rsync libffi-dev python-pip python-dev'
@@ -13,7 +14,7 @@ which apt-add-repository && su -c 'apt-get install graphicsmagick git ruby ruby-
 which pkg && pkg install graphicsmagick git ruby grep file findutils pkg-config libiconv libexif libprotobuf libxslt clang tmux squid make rsync libffi python libcap libcrypt openssl-tool zlib
 
 # Python
-pip install --upgrade pip pygments
+#pip install --upgrade pip pygments
 
 # Ruby
 rm Gemfile.lock
