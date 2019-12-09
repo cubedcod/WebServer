05ef65ceefc96b4d1db11cf1c94c98652a86df14
chi <carmen@replic.net>
2019-12-09 20:59:04 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -21,7 +21,7 @@ image-src
 
     def self.clean body, base
       html = Nokogiri::HTML.fragment body
-      puts "clean #{base}"
+
       # stripped elements
       %w{iframe link[rel='stylesheet'] style link[type='text/javascript'] link[as='script'] script}.map{|s| html.css(s).remove}
       html.css('a[href^="javascript"]').map{|a| a.remove }
@@ -380,26 +380,24 @@ class WebResource
               icon = Icons[p.uri] || slug
               {_: :td, c: (env[:query]||{})['sort'] == p.uri ? icon : {_: :a, class: :head, id: 'sort_by_' + slug, href: '?view=table&sort='+CGI.escape(p.uri), c: icon}}}},
            graph.map{|resource|
-             has_content_row = [Abstract,Content,Image,Video].find{|k|resource.has_key? k}
-             [{_: :tr, c: keys.map{|k|
-                 {_: :td,
-                  c: if k == 'uri'
-                   tCount = 0
-                   [(resource[Title]||[]).map{|title|
-                      title = title.to_s.sub(/\/u\/\S+ on /, '').sub /^Re: /, ''
-                      unless env[:title] == title # show topic if changed from prior post
-                        env[:title] = title; tCount += 1
-                        {_: :a, href: resource['uri'], id: 'r' + Digest::SHA2.hexdigest(rand.to_s), class: 'title', type: 'node', c: CGI.escapeHTML(title)}
-                      end},
-                    ({_: :a, href: resource['uri'], id: 'r' + Digest::SHA2.hexdigest(rand.to_s), class: 'id', type: 'node', c: '&#x1f517;'} if tCount == 0)]
-                 else
-                   (resource[k]||[]).map{|v|value k, v, env }
-                  end}}},
-              ({_: :tr, c: {_: :td, colspan: keys.size,
-                            c: [resource[Abstract] ? [resource[Abstract], '<br>'] : '',
-                                (resource[Image]||[]).map{|i| {style: 'max-width: 28em', c: Markup[Image][i,env]}},
-                                (resource[Video]||[]).map{|i| {style: 'max-width: 32em', c: Markup[Video][i,env]}},
-                                resource[Content]]}} if has_content_row)]}]}
+             {_: :tr, c: keys.map{|k|
+                {_: :td,
+                 c: if k == 'uri'
+                  tCount = 0
+                  [(resource[Title]||[]).map{|title|
+                     title = title.to_s.sub(/\/u\/\S+ on /, '').sub /^Re: /, ''
+                     unless env[:title] == title # show topic if changed from prior post
+                       env[:title] = title; tCount += 1
+                       {_: :a, href: resource['uri'], id: 'r' + Digest::SHA2.hexdigest(rand.to_s), class: 'title', type: 'node', c: CGI.escapeHTML(title)}
+                     end},
+                   ({_: :a, href: resource['uri'], id: 'r' + Digest::SHA2.hexdigest(rand.to_s), class: 'id', type: 'node', c: '&#x1f517;'} if tCount == 0),
+                   resource[Abstract] ? [resource[Abstract], '<br>'] : '',
+                   (resource[Image]||[]).map{|i| {style: 'max-width: 28em', c: Markup[Image][i,env]}},
+                   (resource[Video]||[]).map{|i| {style: 'max-width: 32em', c: Markup[Video][i,env]}},
+                   resource[Content]]
+                else
+                  (resource[k]||[]).map{|v|value k, v, env }
+                 end}}}}]}
     end
 
     # Hash -> Markup
