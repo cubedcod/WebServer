d9515aeca5b3cb4a3d0dc5746561a42ccb9685d0
dell <carmen@replic.net>
2019-12-30 05:06:34 +0000

    p

--- config/site.js
+++ config/site.js
@@ -33,20 +33,22 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // interactivity
     var siteToggle = 1;
-    document.querySelector('#showMain').addEventListener('click',function(e){
-	var elements = document.querySelectorAll('.site');
-	if(siteToggle == 1) { // hide site-elements
-	    siteToggle = 0;
-	    this.style.borderColor = '#333333';
-	    this.style.background = 'repeating-linear-gradient(135deg, #000, #000 .4em, #333 .4em, #333 .8em)';
-	    elements.map(function(l){this.style.display = 'none';});
-	} else {                                  // show site-elements
-	    siteToggle = 1;
-	    this.style.borderColor = '#ffffff';
-	    this.style.background = 'repeating-linear-gradient(135deg, #000, #000 .4em, #fff .4em, #fff .8em)';
-	    elements.map(function(l){this.style.display = 'inline-block';});
-	};
-    });
+    var mainLink = document.getElementById('showMain');
+    if(mainLink)
+	mainLink.addEventListener('click',function(e){
+	    var elements = document.querySelectorAll('.site');
+	    if(siteToggle == 1) { // hide site-elements
+		siteToggle = 0;
+		this.style.borderColor = '#333333';
+		this.style.background = 'repeating-linear-gradient(135deg, #000, #000 .4em, #333 .4em, #333 .8em)';
+		elements.map(function(l){this.style.display = 'none';});
+	    } else {                                  // show site-elements
+		siteToggle = 1;
+		this.style.borderColor = '#ffffff';
+		this.style.background = 'repeating-linear-gradient(135deg, #000, #000 .4em, #fff .4em, #fff .8em)';
+		elements.map(function(l){this.style.display = 'inline-block';});
+	    };
+	});
     document.querySelectorAll('input').map(function(i){
 	this.addEventListener("keydown",function(e){
 	    e.stopPropagation();
