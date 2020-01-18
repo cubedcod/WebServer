ff26d884c0d91866aafee0faef15cb218389fafa
pine <carmen@replic.net>
2020-01-18 00:34:17 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -3,6 +3,60 @@ module Webize
   module HTML
     include WebResource::URIs
 
+    # degunk and reformat to local conventions
+    def self.clean body, base
+      html = Nokogiri::HTML.fragment body
+
+      # strip iframes, stylesheets, scripts and misc gunk
+      html.css('iframe, style, link[rel="stylesheet"], ' + Scripts).remove
+      degunkDoc html
+
+      # tag site-nav elements
+      SiteNav.map{|selector|
+        html.css(selector).map{|node|
+          base.env[:site_chrome] ||= true
+          node['class'] = 'site'}}
+
+      # map image references
+      # CSS:background-image → <img>
+      html.css('[style*="background-image"]').map{|node|
+        node['style'].match(/url\(['"]*([^\)'"]+)['"]*\)/).yield_self{|url|
+          node.add_child "<img src=\"#{url[1]}\">" if url}}
+      # <amp-img> → <img>
+      html.css('amp-img').map{|amp|amp.add_child "<img src=\"#{amp['src']}\">"}
+      # <div> → <img>
+      html.css("div[class*='image'][data-src]").map{|div|
+        div.add_child "<img src=\"#{div['data-src']}\">"}
+
+      html.traverse{|e| # visit node
+        e.attribute_nodes.map{|a| # visit attribute
+
+          # map media references
+          e.set_attribute 'src', a.value if SRCnotSRC.member? a.name
+          e.set_attribute 'srcset', a.value if %w{data-srcset}.member? a.name
+
+          # strip attrs
+          a.unlink if a.name.match?(/^(aria|data|js|[Oo][Nn])|react/) ||
+                      %w(bgcolor height http-equiv layout ping role style tabindex target theme width).member?(a.name)}
+
+        # annotate hrefs
+        if e['href']
+          ref = e['href'].R
+          e.add_child " <span class='uri'>#{CGI.escapeHTML e['href'].sub(/^https?:..(www.)?/,'')[0..127]}</span> " # show full(er) URL
+          e.set_attribute 'id', 'id' + Digest::SHA2.hexdigest(rand.to_s) unless e['id'] # identify node for traversal
+          css = [:uri]; css.push :path if !ref.host || (ref.host == base.host)
+          e['href'] = base.join e['href'] unless ref.host              # resolve relative references
+          e['class'] = css.join ' '                                    # node CSS-class for styling
+        elsif e['id']                                                  # identified node w/ no href
+          e.set_attribute 'class', 'identified'                        # node CSS-class for styling
+          e.add_child " <a class='idlink' href='##{e['id']}'>##{CGI.escapeHTML e['id']}</span> " # link to identified node
+        end
+
+        e['src'] = base.join e['src'] if e['src'] && !e['src'].R.host} # resolve image locations
+
+      html.to_xhtml indent: 0
+    end
+
     def self.webizeValue v, &y
       case v.class.to_s
       when 'Hash'
