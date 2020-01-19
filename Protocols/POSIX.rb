6bec9e1b9769b7b1ae21767bfbdb6fe117d9b77c
pine <carmen@replic.net>
2020-01-19 06:04:08 +0000

    p

--- Protocols/POSIX.rb
+++ Protocols/POSIX.rb
@@ -1,6 +1,7 @@
 %w(fileutils pathname shellwords).map{|d| require d }
 class WebResource
   module POSIX
+
     def fsPath
       (if !host || %w(l localhost).member?(host)
        '.'
@@ -16,19 +17,21 @@ class WebResource
           path
          end)
     end
+
     def glob
-      Pathname.glob(fsPath).map{|p|
-        #join(p.relative_path_from fsPath).R env
-        join(p.relative_path_from node.dirname).R env
-      }
+      Pathname.glob(fsPath).map{|match| join(match.relative_path_from node.dirname).R env}
     end
+
     def node; Pathname.new fsPath end
+
     def shellPath; Shellwords.escape fsPath.force_encoding 'UTF-8' end
+
     def write o
       FileUtils.mkdir_p node.dirname
       File.open(fsPath,'w'){|f|f << o.force_encoding('UTF-8')}
       self
     end
+
   end
   include POSIX
 end
