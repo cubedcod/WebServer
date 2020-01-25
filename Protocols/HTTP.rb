8d100d00077ed316723db0b22156963e8360c5d5
moto <carmen@replic.net>
2020-01-25 17:16:08 +0000

    p

--- Protocols/HTTP.rb
+++ Protocols/HTTP.rb
@@ -20,7 +20,7 @@ class WebResource
     Suffixes = {
       'application/x-javascript' => '.js', 'audio/mpeg' => '.mp3',
       'image/x-icon' => '.ico', 'image/webp' => '.webp',
-      'text/xml' => '.rss'}
+      'text/turtle' => '.ttl', 'text/xml' => '.rss'}
     Internal_Headers = %w(base-uri connection gunk host links path-info query query-string rack.errors rack.hijack rack.hijack? rack.input rack.logger rack.multiprocess rack.multithread rack.run-once rack.url-scheme rack.version rdf remote-addr repository request-method request-path request-uri resp script-name server-name server-port server-protocol server-software site-chrome transfer-encoding unicorn.socket upgrade-insecure-requests ux version via x-forwarded-for)
 
     # handlers
