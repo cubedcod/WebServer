4f108093c5106353882bee7e95e793dd756e61eb
pine <carmen@replic.net>
2020-02-02 22:53:47 +0000

    twitter is stuck in 401/403 hell. give up for now

--- config/site.rb
+++ config/site.rb
@@ -577,9 +577,10 @@ firefox.settings.services.mozilla.com
             uid = json['data']['user']['rest_id']
           end
         end
-        apiURL = 'https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=true&userId=' + uid + '&count=20&ext=mediaStats%2CcameraMoment '
-        apiURL.R(r.env).fetch intermediate: true
-        r.saveRDF.chrono_sort.graphResponse
+        apiURL = 'https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=false&userId=' + uid + '&count=20&ext=mediaStats%2CcameraMoment '
+        [302, {'Location' => apiURL}, []]
+      #apiURL.R(r.env).fetch intermediate: true
+      #r.saveRDF.chrono_sort.graphResponse
       else
         r.upstreamUI.fetch
       end}
