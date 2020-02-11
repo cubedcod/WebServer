5b9cf6940f07803d2ffcd7886f1e3e50a0fda0f8
pine <carmen@replic.net>
2020-02-11 08:32:41 +0000

    fiddle with the twitter crap a bit

--- config/site.rb
+++ config/site.rb
@@ -411,17 +411,20 @@ wired.trib.al).map{|short| GET short, NoQuery }
 
     Twitter = -> r {
       r.env['HTTP_USER_AGENT'] = MobileUA
-      if cookie = r.env['HTTP_COOKIE']
-        attrs = {}
-        cookie.split(';').map{|attr|
-          k , v = attr.split('=').map &:strip
-          attrs[k] = v}
-        r.env['authorization'] = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
-        r.env['x-csrf-token'] = attrs['ct0'] if attrs['ct0']
-        r.env['x-guest-token'] = attrs['gt'] if attrs['gt']
-      end
+
+      setTokens = -> {
+        if cookie = r.env['HTTP_COOKIE']
+          attrs = {}
+          cookie.split(';').map{|attr|
+            k , v = attr.split('=').map &:strip
+            attrs[k] = v}
+          r.env['authorization'] = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
+          r.env['x-csrf-token'] = attrs['ct0'] if attrs['ct0']
+          r.env['x-guest-token'] = attrs['gt'] if attrs['gt']
+        end}
 
       if !r.path || r.path == '/'
+        setTokens[]
         subscriptions = Pathname.glob('twitter/.??*').map{|n|n.basename.to_s[1..-1]}
         subscriptions.shuffle[0..15].each_slice(18){|sub|
           q = sub.map{|u|'from%3A' + u}.join('%2BOR%2B')
@@ -437,24 +440,23 @@ wired.trib.al).map{|short| GET short, NoQuery }
         r.deny
       elsif r.path.match? GlobChars
         r.nodeResponse
-=begin
       elsif r.parts.size == 1 && !r.upstreamUI? && !%w(favicon.ico manifest.json search).member?(r.parts[0])
-
+        setTokens[]
         user = r.parts[0]
         if user.match? /^\d+$/
           uid = user
         else # find uid
-          URI.open('https://api.twitter.com/graphql/G6Lk7nZ6eEKd7LBBZw9MYw/UserByScreenName?variables=%7B%22screen_name%22%3A%22' + user + '%22%2C%22withHighlightedLabel%22%3Afalse%7D', r.headers) do |response|
+          h = r.headers
+          h.delete 'Cookie'
+          h['Accept'] = '*/*'
+          URI.open('https://api.twitter.com/graphql/G6Lk7nZ6eEKd7LBBZw9MYw/UserByScreenName?variables=%7B%22screen_name%22%3A%22' + user + '%22%2C%22withHighlightedLabel%22%3Afalse%7D', h) do |response|
             body = HTTP.decompress response.meta, response.read
             json = ::JSON.parse body
             uid = json['data']['user']['rest_id']
+            puts "found UID #{user} -> #{uid}"
           end
         end
-
-        apiURL = 'https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=false&userId=' + uid + '&count=20&ext=mediaStats%2CcameraMoment'
-        apiURL.R(r.env).fetch intermediate: true
-        r.saveRDF.chrono_sort.graphResponse
-=end
+        ('https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=false&userId=' + uid + '&count=20&ext=mediaStats%2CcameraMoment').R(r.env).fetch
       else
         r.fetch
       end}
