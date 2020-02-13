c9b3157c8a0cb653d570b115ef066f2dae9334e2
pine <carmen@replic.net>
2020-02-13 03:06:44 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -410,7 +410,7 @@ wired.trib.al).map{|short| GET short, NoQuery }
         FileUtils.touch 'twitter/.' + n}}
 
     Twitter = -> r {
-      newUI = true
+      useAPI = !r.upstreamUI?
 
       setTokens = -> {
         if cookie = r.env['HTTP_COOKIE']
@@ -424,7 +424,7 @@ wired.trib.al).map{|short| GET short, NoQuery }
         end}
 
       if !r.path || r.path == '/'
-        if newUI # feed - JSON
+        if useAPI # feed - JSON
           setTokens[]
           subscriptions = Pathname.glob('twitter/.??*').map{|n|n.basename.to_s[1..-1]}
           subscriptions.shuffle.each_slice(18){|sub|
@@ -442,7 +442,7 @@ wired.trib.al).map{|short| GET short, NoQuery }
         r.deny
       elsif r.path.match? GlobChars
         r.nodeResponse
-      elsif newUI && r.parts.size == 1 && !r.upstreamUI? && !%w(favicon.ico manifest.json search).member?(r.parts[0]) # user - JSON
+      elsif useAPI && r.parts.size == 1 && !%w(favicon.ico manifest.json search).member?(r.parts[0]) # user - JSON
         setTokens[]
         user = r.parts[0]
         if user.match? /^\d+$/
@@ -459,11 +459,11 @@ wired.trib.al).map{|short| GET short, NoQuery }
           end
         end
         ('https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=false&userId=' + uid + '&count=20&ext=mediaStats%2CcameraMoment').R(r.env).fetch reformat: true
-      elsif newUI && r.parts.member?('status') # tweet - JSON
+      elsif useAPI && r.parts.member?('status') # tweet - JSON
         setTokens[]
         "https://api.twitter.com/2/timeline/conversation/#{r.parts[-1]}.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&count=20&ext=mediaStats%2CcameraMoment".R(r.env).fetch reformat: true
       else # user/tweet - HTML
-        setTokens[] if r.host == 'api.twitter.com'
+        setTokens[] if useAPI && r.host == 'api.twitter.com'
         r.fetch
       end}
 
