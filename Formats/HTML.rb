863cb83932d029206d16da5fe9ba5f101fb26c96
pine <carmen@replic.net>
2020-03-06 09:49:02 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -46,47 +46,31 @@ module Webize
     # format to local conventions
     def self.format body, base
       html = Nokogiri::HTML.fragment body
-
-      # clean misc gunk
       html.css('iframe, style, link[rel="stylesheet"], ' + Scripts).remove
-      degunkDoc html
-
-      # map image references
-      # CSS:background-image → <img>
-      html.css('[style*="background-image"]').map{|node|
-        node['style'].match(/url\(['"]*([^\)'"]+)['"]*\)/).yield_self{|url|
+      degunkDoc html                                                   # remove misc gunk
+      html.css('[style*="background-image"]').map{|node|               # map image references to classic image-tag
+        node['style'].match(/url\(['"]*([^\)'"]+)['"]*\)/).yield_self{|url| # image in CSS background
           node.add_child "<img src=\"#{url[1]}\">" if url}}
-      # <amp-img> → <img>
-      html.css('amp-img').map{|amp|amp.add_child "<img src=\"#{amp['src']}\">"}
-      # <div> → <img>
-      html.css("div[class*='image'][data-src]").map{|div|
-        div.add_child "<img src=\"#{div['data-src']}\">"}
-
+      html.css('amp-img').map{|amp|amp.add_child "<img src=\"#{amp['src']}\">"} # image in amp or div tag
+      html.css("div[class*='image'][data-src]").map{|div|div.add_child "<img src=\"#{div['data-src']}\">"}
       html.traverse{|e| # visit node
         e.attribute_nodes.map{|a| # visit attribute
-
-          # map media references
-          e.set_attribute 'src', a.value if SRCnotSRC.member? a.name
+          e.set_attribute 'src', a.value if SRCnotSRC.member? a.name   # map @src attribute to classic name
           e.set_attribute 'srcset', a.value if %w{data-srcset}.member? a.name
-
-          # strip attrs
-          a.unlink if a.name.match?(/^(aria|data|js|[Oo][Nn])|react/) ||
+          a.unlink if a.name.match?(/^(aria|data|js|[Oo][Nn])|react/)||# strip attributes
                       %w(bgcolor class height http-equiv layout ping role style tabindex target theme width).member?(a.name)}
-
-        # annotate hrefs
-        if e['href']
-          ref = e['href'].R
-          e.add_child " <span class='uri'>#{CGI.escapeHTML e['href'].sub(/^https?:..(www.)?/,'')[0..127]}</span> " # show full(er) URL
-          e.set_attribute 'id', 'id' + Digest::SHA2.hexdigest(rand.to_s) unless e['id'] # identify node for traversal
-          css = [:uri]; css.push :path if !ref.host || (ref.host == base.host)
+        if e['href']                                                   # resolve and annotate links
+          ref = e['href'].R                                            # show full(er) URL in text
+          e.add_child " <span class='uri'>#{CGI.escapeHTML e['href'].sub(/^https?:..(www.)?/,'')[0..127]}</span> "
+          e.set_attribute 'id', 'id' + Digest::SHA2.hexdigest(rand.to_s) unless e['id'] # identify node
+          css = [:uri]; css.push :path if !ref.host || (ref.host == base.host) # local site or global link
           e['href'] = base.join e['href'] unless ref.host              # resolve relative references
-          e['class'] = css.join ' '                                    # node CSS-class for styling
-        elsif e['id']                                                  # identified node w/ no href
-          e.set_attribute 'class', 'identified'                        # node CSS-class for styling
-          e.add_child " <a class='idlink' href='##{e['id']}'>##{CGI.escapeHTML e['id']}</span> " # link to identified node
+          e['class'] = css.join ' '                                    # node CSS-class
+        elsif e['id']                                                  # identified node without a link
+          e.set_attribute 'class', 'identified'                        # node CSS-class
+          e.add_child " <a class='idlink' href='##{e['id']}'>##{CGI.escapeHTML e['id']}</span> " # add link to node
         end
-
-        e['src'] = base.join e['src'] if e['src'] && !e['src'].R.host} # resolve image locations
+        e['src'] = base.join e['src'] if e['src'] && !e['src'].R.host} # resolve media location
 
       html.to_xhtml indent: 0
     end
