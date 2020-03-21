61e7820ff3f0ae9d62b1cafd74d03776880b9073
pine <carmen@replic.net>
2020-03-21 22:30:03 +0000

    p

--- Formats/Video.rb
+++ Formats/Video.rb
@@ -1,4 +1,31 @@
 module Webize
+  module MOV
+    class Format < RDF::Format
+      content_type 'video/quicktime', :extensions => [:mov,:MOV]
+      reader { Reader }
+    end
+
+    class Reader < RDF::Reader
+      include WebResource::URIs
+      format Format
+
+      def initialize(input = $stdin, options = {}, &block)
+        @subject = (options[:base_uri] || '#mp3').R
+        if block_given?
+          case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+          end
+        end
+        nil
+      end
+
+      def each_triple &block; each_statement{|s| block.call *s.to_triple} end
+
+      def each_statement &fn
+      end
+    end
+  end
   module MP4
     class Format < RDF::Format
       content_type 'video/mp4', :extension => :mp4
