8c9707fb7f1d419a0c1fbc743425e2d1413545cc
pine <carmen@replic.net>
2020-03-25 15:49:04 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -62,11 +62,13 @@ module Webize
       html.css('amp-img').map{|amp|amp.add_child "<img src=\"#{amp['src']}\">"}                            # amp image
       html.css("div[class*='image'][data-src]").map{|div|div.add_child "<img src=\"#{div['data-src']}\">"} # div image
 
-      # <p> <pre>
+      # <p> <pre> <ul>
       base.env[:pCount] ||= -1
       base.env[:preCount] ||= -1
-      html.css('p').map{|e|   e.set_attribute 'id', 'p'   + (  base.env[:pCount] += 1).to_s unless e['id']}
+      base.env[:ulCount] ||= -1
+      html.css('p').map{|e|   e.set_attribute 'id', 'p'   + (base.env[:pCount]   += 1).to_s unless e['id']}
       html.css('pre').map{|e| e.set_attribute 'id', 'pre' + (base.env[:preCount] += 1).to_s unless e['id']}
+      html.css('ul').map{|e|  e.set_attribute 'id', 'ul'  + (base.env[:ulCount]  += 1).to_s unless e['id']}
 
       # <*>
       html.traverse{|e|
