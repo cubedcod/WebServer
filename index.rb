7978ab676848a90c9e7846f97bb4a49c763cb330
kohaku <carmen@replic.net>
2020-05-11 03:00:45 +0000

    add Archive.rb for compressed files

--- index.rb
+++ index.rb
@@ -1,4 +1,4 @@
-{Formats: %w(RDF Audio Calendar Feed HTML Image JS Mail MIME PDF Text Video),
+{Formats: %w(RDF Archive Audio Calendar Feed HTML Image JS Mail MIME PDF Text Video),
  Protocols: %w(POSIX HTTP),
  config: %w(gunk meta site)}.
   map{|cat, components| components.map{|component|
