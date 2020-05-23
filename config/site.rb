0c6f7d8e04ce47e559b911f20e7674a94e13a3cc
kohaku <carmen@replic.net>
2020-05-23 07:42:26 +0000

    delegate access-tokken minting to twitter UI

--- config/site.rb
+++ config/site.rb
@@ -237,8 +237,16 @@ graphql.api.dailymotion.com).map{|h| Allow h}
         "https://api.twitter.com/2/search/adaptive.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&q=%23#{r.parts[1]}&count=20&query_source=&pc=1&spelling_corrections=1&ext=mediaStats%2ChighlightedLabel%2CcameraMoment".R(r.env).fetch reformat: true
       else
         NoGunk[r]
-      end}
-    %w(mobile www).map{|h| GET h + '.twitter.com', -> r {[302, {'Location' => 'https://twitter.com' + r.path}, []]}}
+      end.yield_self{|s,h,b|
+        if s == 429 && !r.upstreamUI? # load upstream UI to mint new tokens
+          [302, {'Location' => '?UI=upstream'}, []]
+        else
+          [s,h,b]
+        end}}
+
+    %w(mobile www).map{|host|
+      GET host + '.twitter.com', -> r {
+        [302, {'Location' => 'https://twitter.com' + r.path}, []]}}
 
     # Yahoo
     GET 'news.yahoo.com'
