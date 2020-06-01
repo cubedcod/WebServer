f35353fb1f64313c0d82fb7c821938e0116dacda
kohaku <carmen@replic.net>
2020-06-01 09:58:13 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -126,7 +126,6 @@ graphql.api.dailymotion.com).map{|h| Allow h}
       NoGunk[r]}
 
     # Google
-    NoProxy = -> r {r.parts[0] == 'proxy' ? r.deny(200,:image) : NoGunk[r]}
     GoAU =  -> r {
       if url = (r.query_values || {})['adurl']
         dest = url.R
@@ -138,14 +137,18 @@ graphql.api.dailymotion.com).map{|h| Allow h}
     GET 'googleads.g.doubleclick.net', GoAU
     GET 'googleweblight.com', GotoURL
     GET 'www.googleadservices.com', GoAU
+
     if ENV.has_key? 'GOOGLE'
+      GET 'www.google.com', -> r {%w(async complete).member?(r.parts[0]) ? r.deny : (r.path == '/url' ? GotoURL : NoGunk)[r]}
       %w(aa books groups).map{|h|                                                        Allow h + '.google.com' }
       %w(docs images kh khms0 khms1 khms2 khms3 lh3 maps photos).map{|h|                   GET h + '.google.com' }
       %w(encrypted-tbn0 encrypted-tbn1 encrypted-tbn2 encrypted-tbn3 maps ssl www).map{|h| GET h + '.gstatic.com' }
       %w(maps storage).map{|h|                                                             GET h + '.googleapis.com' }
+      NoProxy = -> r {r.parts[0] == 'proxy' ? r.deny(200,:image) : NoGunk[r]}
       (3..6).map{|i| GET "lh#{i}.googleusercontent.com", NoProxy}
-      GET 'www.google.com', -> r {%w(async complete).member?(r.parts[0]) ? r.deny : (r.path == '/url' ? GotoURL : NoGunk)[r]}
       GET 'yt3.ggpht.com', NoProxy
+    else
+      GET 'www.google.com', -> r {r.path == '/search' ? r.fetch : r.deny}
     end
 
     # Imgur
