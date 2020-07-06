f36faba2105e4c87112135681862de104fa779c3
kohaku <carmen@replic.net>
2020-07-06 19:48:51 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -34,7 +34,7 @@ o(m(niture|tr)|nesignal|pt(anon|imera)|utbrain)|
 p(erimeter-?x|i(wik|xel(propagate)?)|lacement|op(down|over|up)|orpoiseant|owaboot|repopulator|ro(fitwell|m(o(tion)?s?|pt))|ub(exchange|matic))|/pv|
 quantcast|
 recaptcha|record(event|stats?)|re?t(ar)?ge?t(ing)?|(rich)?relevance|recirc.*|rubicon.*|
-s(a(fe[-_]?browsing|ilthru)|erv(edby|ice[-_]?worker)|(har|tag)e(aholic|count|daddy)|i(ftscience|gnalr|tenotice)|ponsor(ed)?|w.js)|
+s(a(fe[-_]?browsing|ilthru)|erv(edby|ice[-_]?worker)|(har|tag)e(aholic|count|daddy)|i(ftscience|gnalr|tenotice)|ourcepoint|ponsor(ed)?|w.js)|
 t(aboola.*|(arget|rack)(ers?|ing).*|ampering|ealium|elemetry|inypass|ra?c?k?ing(data)?|ricorder|rustx|ype(face|kit))|autotrack|
 u(rchin|s(abilla|er[-_]?(context|location))|tm)|
 web(font|trends)|wp-?(json|rum)|
