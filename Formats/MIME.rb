64ff0b9f406f41016942b39cd72265e44393a367
kohaku <carmen@replic.net>
2020-07-21 05:10:15 +0000

    p

--- Formats/MIME.rb
+++ Formats/MIME.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
+require 'rack'
 class WebResource
   module URIs
 
--- index.rb
+++ index.rb
@@ -1,5 +1,5 @@
 {Formats: %w(RDF Archive Audio Calendar Feed HTML Image JSON Mail MIME PDF Text Video),
  Protocols: %w(POSIX HTTP),
  config: %w(meta site gunk)}.
-  map{|cat, components| components.map{|component|
-    require_relative "#{cat}/#{component}"}}
+  map{|category, components| components.map{|component|
+    require_relative "#{category}/#{component}"}}
