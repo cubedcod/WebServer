c6dc6feae07a29887d6c741540e0f8b8e56b445c
kohaku <carmen@replic.net>
2020-07-24 04:09:57 +0000

    p

--- Formats/CSV.rb
+++ Formats/CSV.rb
@@ -18,30 +18,39 @@ class WebResource
 
       {_: :table, class: :tabular,
        c: [{_: :thead,
-            c: {_: :tr, c: keys.map{|p|
+            c: {_: :tr, c: keys.map{|p|                                          # header
                   p = p.R
                   slug = p.fragment || (p.path && p.basename) || ' '
                   icon = Icons[p.uri] || slug
-                  [{_: :th, c: {_: :a, id: 'sort_by_' + slug, href: '?view=table&sort='+CGI.escape(p.uri), c: icon}}, "\n"]}}}, "\n",
+                  [{_: :th,
+                    c: {_: :a, id: 'sort_by_' + slug, href: '?view=table&sort='+CGI.escape(p.uri), c: icon}}, "\n"]}}}, "\n", # sorting link
            {_: :tbody,
             c: graph.map{|resource|
 
-              re = (resource['uri'] || ('#' + Digest::SHA2.hexdigest(rand.to_s))).R env
-              local_id = re.path == env['REQUEST_PATH'] && re.fragment || ('r' + Digest::SHA2.hexdigest(re.uri))
+              re = (resource['uri'] || ('#' + Digest::SHA2.hexdigest(rand.to_s))).R env                      # resource identity
+              local_id = re.path == env[:base].path && re.fragment || ('r' + Digest::SHA2.hexdigest(re.uri)) # row identity
 
-              [{_: :tr, id: local_id, c: keys.map{|k|
+              [{_: :tr, id: local_id, c: keys.map{|k|                            # row
                  [{_: :td, property: k,
                   c: if k == 'uri'
                    tCount = 0
                    [(resource[Title]||[]).map{|title|
                       title = title.to_s.sub(/\/u\/\S+ on /, '').sub /^Re: /, ''
-                      unless env[:title] == title # show topic if changed from previous post
+                      unless env[:title] == title
                         env[:title] = title; tCount += 1
-                        {_: :a, href: re.href, class: :title, type: :node, c: CGI.escapeHTML(title), id: 'r' + Digest::SHA2.hexdigest(rand.to_s)}
+                        {_: :a, href: re.href,                                   # link to row with title (changed from prior row)
+                         class: :title,
+                         type: :node,
+                         c: CGI.escapeHTML(title), id: 'r' + Digest::SHA2.hexdigest(rand.to_s)}
                       end},
-                    ({_: :a, href: re.href, class: :id, type: :node, c: '🔗', id: 'r' + Digest::SHA2.hexdigest(rand.to_s)} if tCount == 0),
+                    ({_: :a, href: re.href,                                      # link to row
+                      class: :id,
+                      type: :node,
+                      c: '🔗',
+                      id: 'r' + Digest::SHA2.hexdigest(rand.to_s)} if tCount == 0),
                     (resource[SIOC+'reply_of']||[]).map{|r|
-                      {_: :a, href: r.to_s, c: Icons[SIOC+'reply_of']} if r.class == RDF::URI || r.class == WebResource},
+                      {_: :a, href: r.to_s,
+                       c: Icons[SIOC+'reply_of']} if r.class == RDF::URI || r.class == WebResource},
                     resource[Abstract] ? [resource[Abstract], '<br>'] : '',
                     [Image,
                      Video].map{|t|(resource[t]||[]).map{|i|
