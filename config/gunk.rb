7117d75dcb62ef39eedaada189efe57aa7621344
kohaku <carmen@replic.net>
2020-08-01 03:34:27 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -17,7 +17,7 @@ class WebResource
     Gunk = %r([-._\/'"\s:?&=~%](
 affiliate(link)?s?|ad(obe|s|toniq|vert.*)?|ak(am|ismet)|.*analytics.*|apester|appnexus|atrk|audience|(app|smart)?
 b(lueconic|ouncee?x.*|ugsnag)|.*bid(d(er|ing).*|s)|
-c(edexis|hartbeat.*|mp|ollector|omscore|on(sent|version)|ookie.?(c(hoice|onsent)|law|notice)|riteo|xense)|
+c(edexis|hartbeat.*|mp|ollector|omscore|onsent|ookie.?(c(hoice|onsent)|law|notice)|riteo|xense)|
 de(mandware|t(ect|roitchicago))|dfp|dis(ney(id)?|qus)|doubleclick|
 e(moji|nsighten|proof|scenic|vidon|zoic)|
 fbcdn.*gif|frosmo|
