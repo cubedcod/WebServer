05106dbe53032a1ba849cd1a968c0230a6ffee20
kohaku <carmen@replic.net>
2020-08-21 11:38:38 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -32,7 +32,7 @@ keywee|kr(ux|xd).*|
 (app|s)?m(a(ilchimp|r(feel|keto)|tomo|utic)|etrics?|ms|pulse|tr)|
 newrelic|
 o(m(niture|tr)|nesignal|pt(anon|imera)|utbrain)|
-p(erimeter-?x|iwik|op(down|over|up)|orpoiseant|owaboot|repopulator|ro(fitwell|m(o(tion)?s?|pt))|ub(exchange|matic))|/pv|
+p(erimeter-?x|iwik|op(down|over|up)|orpoiseant|owaboot|repopulator|ro(fitwell|mo(tion)?s?)|ub(exchange|matic))|/pv|
 quantcast|
 recaptcha|record(event|stats?)|re?t(ar)?ge?t(ing)?|(rich)?relevance|recirc.*|rubicon.*|
 s(a(fe[-_]?browsing|ilthru)|erv(edby|ice[-_]?worker)|(har|tag)e(aholic|count|daddy)|i(ftscience|gnalr|tenotice)|ourcepoint|ponsor(ed)?|w.js)|
