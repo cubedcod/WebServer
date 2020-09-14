d2d9c3891063e867b6ca5dcf287110cd3355d1a9
kohaku <carmen@replic.net>
2020-09-14 06:52:43 +0000

    morning gunk II

--- Formats/Text.rb
+++ Formats/Text.rb
@@ -281,7 +281,7 @@ module Webize
             subject = graph.join '#msg'
             yield subject, Type, Post.R, graph
             yield subject, Date, date, graph
-            yield subject, Content, Webize::HTML.format(msg.hrefs, @base), graph
+            yield subject, Content, Webize::HTML.format(msg.hrefs, @base), graph if msg
             yield subject, Creator, (@base.host + dirname).split(/\W/).join('.'), graph
             yield subject, To, @base, graph
           }
