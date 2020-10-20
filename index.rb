44e847ab237b410ece72f7e12e8610f8a548060b
kohaku <carmen@replic.net>
2020-10-20 03:44:30 -0400

    add still-unimplemented Gemini.rb as a reminder

--- index.rb
+++ index.rb
@@ -1,5 +1,5 @@
 {Formats: %w(URI Archive Audio Calendar Code CSS CSV Feed HTML Image JSON Mail Markdown MIME PDF RDF Subtitle Text Video),
- Protocols: %w(POSIX HTTP),
+ Protocols: %w(Gemini POSIX HTTP NNTP),
  config: %w(gunk meta site)}.
   map{|category, components| components.map{|component|
     require_relative "#{category}/#{component}"}}
