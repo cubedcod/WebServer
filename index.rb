3961dc99a44779a70f1e45a8844c7e169fe8232b
user <carmen@replic.net>
2020-12-09 07:54:02 +0000

    p

--- index.rb
+++ index.rb
@@ -1,5 +1,10 @@
 {Formats: %w(URI Archive Audio Calendar Chat Code CSS CSV Feed HTML Image JSON Mail Markdown MIME PDF RDF Subtitle Text Video),
  Protocols: %w(Gemini POSIX HTTP NNTP),
  config: %w(gunk meta site)}.
-  map{|category, components| components.map{|component|
+  map{|category, components|
+  components.map{|component|
     require_relative "#{category}/#{component}"}}
+
+module Webize
+  Verbose = ENV.has_key? 'Verbose'
+end
