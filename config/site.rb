91ee0a02ad27dcea6a19fa93ffafda9d8bc426b8
user <carmen@replic.net>
2021-01-16 16:20:29 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -134,9 +134,7 @@ l.facebook.com l.instagram.com
       elsif path.index('/amp/s/') == 0
         [302, {'Location' => 'https://' + r.path[7..-1]}, []]
       elsif p == 'complete'
-        qs = r.query_values || {}
-        q = qs['q']
-        r.fetch.yield_self{|s,h,b| puts s,h,b} if qs['client'] == 'firefox'
+        q = (r.query_values || {})['q']
         output = ")]}'\n" + [q,["http://localhost:8000/h","http://localhost:8000/d","http://localhost:8000/m",
                                 "https://twitter.com",
                                 "https://www.reddit.com/r/androidx86+blissos+chrultrabook+chromeos+stallmanwasright/new",
