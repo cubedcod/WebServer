c5b83a3aca8a0d0220ece34d04fb185d964cd5f8
user <carmen@replic.net>
2021-01-20 15:44:41 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -590,16 +590,17 @@ l.facebook.com l.instagram.com
 
     doc.css('div.comment[id]').map{|comment|
       post_id, avatar, author, post_link = comment.css('.byline > a')
-      subject = (join post_link['href']).R
-      graph = subject.join [subject.basename, subject.fragment].join '.'
-      yield subject, Type, Post.R, graph
-      yield subject, To, (join subject.path), graph
-      yield subject, Creator, (join author['href']), graph
-      yield subject, Creator, author.inner_text, graph
-      yield subject, Image, (join avatar.css('img')[0]['src']), graph
-      yield subject, Date, Time.parse(comment.css('.byline > span[title]')[0]['title']).iso8601, graph
-      yield subject, Content, (Webize::HTML.format comment.css('.comment_text')[0], self), graph
-
+      if post_link
+        subject = (join post_link['href']).R
+        graph = subject.join [subject.basename, subject.fragment].join '.'
+        yield subject, Type, Post.R, graph
+        yield subject, To, (join subject.path), graph
+        yield subject, Creator, (join author['href']), graph
+        yield subject, Creator, author.inner_text, graph
+        yield subject, Image, (join avatar.css('img')[0]['src']), graph
+        yield subject, Date, Time.parse(comment.css('.byline > span[title]')[0]['title']).iso8601, graph
+        yield subject, Content, (Webize::HTML.format comment.css('.comment_text')[0], self), graph
+      end
       comment.remove }
   end
 
