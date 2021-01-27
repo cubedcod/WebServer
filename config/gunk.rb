87ab5d852a19b32c66680d72c7960337e7adcbd1
user <carmen@replic.net>
2021-01-27 23:36:28 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -20,10 +20,10 @@ class WebResource
     Gunk = Regexp.new SiteDir.join('gunk.regex').read.chomp, Regexp::IGNORECASE
 
     ScriptGunk = %r([-._\/\(\)'"\s:?&=~%](ads|b0e8|bing|gt(ag|m)|pi(ng|xel)|s(entry|umo)|t(aw|rac)k|utm)[-._\/\(\)'"\s:?&=~%]|
-ad(.?bl(ade|ock)|push|r(ecover|oll)|sense|vert)|am(azon|plitude)|acsb|addtoany|algolia|analytic|aswpsdkus|auction|
+ad(.?bl(ade|ock)|push|r(ecover|oll)|sense|vert)|am(azon|plitude)|acsb|addtoany|algolia|analytic|aswpsdk|auction|
 baidu|bazaarvoice|bidder|BO(OMR|uncee?x)|browser.?update|btncdn|bu(gherd|zzfeed)|
 campaign|chartbeat|chimp|cl(ick(cea|fu)se|oudfront)|co(mscore|n(fiant|natix|sent)|okie.?(consent|law))|cpx\.|cr(iteo|sspxl|wdcntrl)|cxense|
-detroitchicago|[^a-z]dfp|disqus|dmpxs|dotmetrics|doubleclick|
+datadog|detroitchicago|[^a-z]dfp|disqus|dmpxs|dotmetrics|doubleclick|
 effectivemeasure|ensighten|evidon|Ezoic|
 fa(cebook|stclick)|feedbackify|fingerprint|foresee|fullstory|funnel|
 gdpr|ge(oloc|t(drip|pocket))|google|grapeshot|gumgum|gwallet|
@@ -32,7 +32,7 @@ im(pression|rworldwide)|inte(llitxt|rcom)|
 kr(ux|xd)|kochava|
 lexity|li(nkedin|strak|veperson)|
 ma(iler|rket|tomo)|me(dia\.net|quoda|tri(cs|x))|ml314|mo(dal|useflow)|mpulse|
-narrativ\.|newrelic|newsletter|npttech|nreum|
+narrativ\.|newrelic|newsletter|notif[iy]|npttech|nreum|
 olark|omappapi|one(signal|trust)|op(t?n?mn?str|t(anon|imize))|outbrain|owneriq|
 pa(ges(ense|peed)|r(dot|sely)|y(pa|wal)l)|pi(n(gdom|img|terest)|wik)|porpoiseant|pr(ebid|omo)|pub(\.network|m(atic|ine))|pushly|
 quora|qua(ltrics|nt(cast|serv|um))|
