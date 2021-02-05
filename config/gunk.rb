ecac28d97b47d06543d1242fc5fe257b448d4915
user <carmen@replic.net>
2021-02-05 08:23:48 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -22,20 +22,20 @@ class WebResource
     ScriptGunk = %r([-._\/\(\)'"\s:?&=~%](6sc|acsbapp?|b0e8|dmpxs|fyre|gaq|kr(ux|xd)|licdn|marketo|sumo|tawk|turnto|zqtk)[-._\/\(\)'"\s:?&=~%]|
 ad(blade|r(ecover|oll)|dthis)|alexametrics|am(azon-adsystem|plitude)\.com|addtoany|algolia|aswpsdk|audioeye|
 baidu|bazaarvoice|bing\.com|BO(OMe?R(ang)?|uncee?x)|browser.?update|btncdn|bu(gherd|zzfeed)|
-chartbeat|chimp|cl(ick(cea|fu)se)|co(mscore\.com|n(fiant|n(atix|ect\.facebook))|okie.?(consent|law))|cpx\.|cr(i(sp\.chat|teo)|sspxl|wdcntrl)|cxense|
+chartbeat|chimp|cl(ick(cea|fu)se)|co(mscore\.com|n(fiant|natix)|okie.?(consent|law))|cpx\.|cr(i(sp\.chat|teo)|sspxl|wdcntrl)|cxense|
 datadog|de(mdex|troitchicago)|disqus(cdn)?\.com|dotmetrics|
 effectivemeasure|ensighten|evidon|Ezoic|
-fastclick|firebase|foresee|fullstory|funnel|
-gaug\.es|g[eo]t(chosen|drip|pocket)|google.?(analytics|tag(manager|services))\.com|grapeshot|gumgum\.com|
+fastclick|firebase|foresee|fullstory|
+gaug\.es|g[eo]t(chosen|drip|pocket)|grapeshot|gumgum\.com|
 hotjar|hs-analytics|hubspot|
-in(folink|te(llitxt|rcom))|iubenda|
+in(folink|te(llitxt|rcom\.(com|io)))|iubenda|
 kochava|
 lexity|li(strak|veperson)|
 ma(rfeel|tomo)|me(dia\.net|quoda)|ml314|mouseflow|mpulse|
 narrativ\.|newrelic|npttech|nreum|
 olark|omappapi|one(signal|trust)|online-metrix|op(t?n?mn?str|t(anon|imizely))|outbrain|owneriq|
-pa(ges(ense|peed)|r(dot|sely)|y(pal\.com|wall))|petametrics|pi(co\.tools|n(gdom|img)|wik)|porpoiseant|prebid|pub(\.network|m(atic\.com|ine))|push(ly|nami)|
-quora|qua(l(aroo|trics)|nt(cast|serv))|
+pa(ges(ense|peed)|r(dot|sely)|ypal\.com)|petametrics|pi(co\.tools|n(gdom|img)|wik)|porpoiseant|prebid|pub(\.network|m(atic\.com|ine))|push(ly|nami)|
+quora|qua(l(aroo|trics)|nt(cast|serve?)\.com)|
 radiateb2b|r-login|rightmessage|rlcdn|
 sa(il.?(horizon|thr)|les(force|loft))|sc(ene7|orecardresearch)|se(archiq|edtag|gment\.(com|io)|ntry-cdn|rvedby|ssioncam)|shopify|slickstream|smart(asset|look)|snowplow|spot\.im|statcounter|
 taboola|ti(qcdn|nypass)|toutapp|trustpilot|typekit|
@@ -43,7 +43,7 @@ unruly|
 venatus|viglink|vuukle|vntsm|
 woocommerce|wp.?(emoji|groho|rum)|
 yandex|\.yimg|yotpo|
-ze(ndesk|rgnet)|zo(ominfo|pim))xi
+zergnet|zo(ominfo|pim))xi
 
   end
 end
