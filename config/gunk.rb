11af6408f32d5600663c5f34fbcf0864324bc2cd
user <carmen@replic.net>
2021-02-13 05:45:25 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -19,7 +19,7 @@ class WebResource
 
     Gunk = Regexp.new SiteDir.join('gunk.regex').read.chomp, Regexp::IGNORECASE
 
-    ScriptGunk = %r([-._\/\(\)'"\s:?&=~%](6sc|acsbapp?|ad(miral|s)?|aksb|b0e8|dmpxs|en25|fyre|g(aq|t(ag|m))|kr(ux|xd)|licdn|m(ar(insm|keto)|pulse)|nr-data|o(mtrdc|penx)|paq|s(hare[a-z]*|umo)|t(aw|rac)k[a-z]*|turnto|zqtk)[-._\/\(\)'"\s:?&=~%]|
+    ScriptGunk = %r([-._\/\(\)'"\s:?&=~%](6sc|acsbapp?|ad(miral|s)?|aksb|b0e8|d([fm]p(xs)?)|en25|fyre|g(aq|t(ag|m))|kr(ux|xd)|licdn|m(ar(insm|keto)|pulse)|nr-data|o(mtrdc|penx)|paq|s(hare[a-z]*|umo)|t(aw|rac)k[a-z]*|turnto|zqtk)[-._\/\(\)'"\s:?&=~%]|
 ad.?(bl(ade|ock[a-z]*)|r(ecover|oll)|dthis|layer|s(afeprotected|ense|lot)|unit|zone)|alexametrics|am(azon[a-z]*|plitude)\.com|analytics|addtoany|algolia|app(dynamics|nexus)|apstag|aswpsdk|au(ction|dioeye)|
 bazaarvoice|beacon|bi(d(d(er|ing)|s)[a-z]*|ng\.com)|BO(mbora|OMe?R(ang)?|uncee?x)|browser.?update|btncdn|bu(gherd|zzfeed)|
 campaign|ch(artbeat|impstatic)|cl(arity\.ms|ick(cea|fu )se)|co(mscore\.com|n(fiant|natix|sent|vers(ant|ion))|okie.?[a-z]*)|cpx\.|cr(i(sp\.chat|teo)|sspxl|wdcntrl)|cxense|
@@ -37,7 +37,7 @@ olark|omappapi|one(signal|trust)|online-metrix|op(t?n?mn?str|t(anon|imizely\.com
 pa(ge(s(ense|peed)|.?view)|r(dot|sely)|y[pw]all?)|pbjs|pe(r(formance|imeter.?x|mutive|sonaliz[a-z]*)|tametrics)|pi(ano\.io|co\.tools|n(gdom|img|terest)|wik)|po(rpoiseant|strelease)|prebid[a-z]+|pub(\.network|m(atic|ine))|push(bullet|ly|nami)|px-cloud|
 quora|qua(l(aroo|trics)|nt(cast|serve?)\.)|
 radiateb2b|r-login|revcontent|rightmessage|rlcdn|rubicon|
-sa(il.?(horizon|thr[a-z]+)|lesloft\.com)|sc(ene7|(arab|orecard)research)|se(archiq|edtag|gment\.(com|io)|ntry-cdn|rv(edby|ice.?worker)|ssioncam)|shopify|signup|slickstream|smart(asset|look)|snowplow|so(cial|nobi|vrn)|spo(nsor[a-z]*|t.?im)|statcounter|swoop\.com|
+sa(il.?(horizon|thr[a-z]+)|lesloft\.com)|sc(ene7|(arab|orecard)research)|se(archiq|edtag|gment\.(com|io)|ntry-cdn|rv(edby|ice.?worker)|ssioncam)|shopify|signup|slickstream|smart(asset|look)|snowplow|so(cial|nobi|vrn)|spo(nsor[a-z]*|t.?im)|statcounter|swoop\.com|synacor[a-z]*|
 ta(boola|rgeting)|tempest|ti(qcdn|nypass)|thinglink|toutapp|[a-z]*tr(ack(cmp|er|ing)[a-z]*|iplelift|ust(pilot|x))|twitter|typekit|
 unruly|urbanairship|us(abilla|er.?agent)|
 ve(natus|rizon)|viglink|vuukle|vntsm|
