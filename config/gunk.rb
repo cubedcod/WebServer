d3885a290e8ec0c3a92a9097717e8443b73e66ec
user <carmen@replic.net>
2021-02-13 11:09:56 +0000

    p

--- config/gunk.rb
+++ config/gunk.rb
@@ -22,7 +22,7 @@ class WebResource
     ScriptGunk = %r([-._\/\(\)'"\s:?&=~%](6sc|acsbapp?|ad(miral|s)?|aksb|b0e8|d([fm]p(xs)?)|en25|fyre|g(aq|t(ag|m))|kr(ux|xd)|licdn|m(ar(insm|keto)|pulse)|nr-data|o(mtrdc|penx)|paq|s(hare[a-z]*|umo)|t(aw|rac)k[a-z]*|turnto|zqtk)[-._\/\(\)'"\s:?&=~%]|
 ad.?(bl(ade|ock[a-z]*)|r(ecover|oll)|dthis|layer|s(afeprotected|ense|lot)|unit|zone)|alexametrics|am(azon[a-z]*|plitude)\.com|analytics|addtoany|algolia|app(dynamics|nexus)|apstag|aswpsdk|au(ction|dioeye)|
 bazaarvoice|beacon|bi(d(d(er|ing)|s)[a-z]*|ng\.com)|BO(mbora|OMe?R(ang)?|uncee?x)|browser.?update|btncdn|bu(gherd|zzfeed)|
-campaign|ch(artbeat|impstatic)|cl(arity\.ms|ick(cea|fu )se)|co(mscore\.com|n(fiant|natix|sent|vers(ant|ion))|okie.?[a-z]*)|cpx\.|cr(i(sp\.chat|teo)|sspxl|wdcntrl)|cxense|
+campaign|ch(artbeat|impstatic)|cl(arity\.ms|ick(cea|fu )se)|co(mscore\.com|n(fiant|natix|sent|versant)|okie.?[a-z]*)|cpx\.|cr(i(sp\.chat|teo)|sspxl|wdcntrl)|cxense|
 datadog\.com|de(m(andbase|dex)|troitchicago)|dis(qus(cdn)?\.com|trictm)|do(ubleclick|t(metrics|omi))|
 effectivemeasure|email|ensighten|evidon|\.ex\.co|Ezoic|
 fa(cebook|stclick)|feedbackify|firebase|foresee\.com|freshchat|fullstory|(function|var)[\s\(]+_0x|
@@ -32,14 +32,14 @@ ibclick\.stream|impression[a-z]*|in(folink|stagram|te(llitxt|r(com\.(com|io)|sti
 kochava|
 lexity\.com|li(nkedin|strak|veperson)|
 ma(iler[a-z]*|r(feel|keting)|t(htag|omo)|ven\.io)|me(dia\.net|quoda)|ml314|mouseflow|munchkin|mxpnl|
-narrativ\.|new(relic|sletter)|noti[cf][a-z]+|npttech|nreum|
+narrativ\.|new(relic|sletter)|npttech|nreum|
 olark|omappapi|one(signal|trust)|online-metrix|op(t?n?mn?str|t(anon|imizely\.com))|outbrain|owneriq|
 pa(ge(s(ense|peed)|.?view)|r(dot|sely)|y[pw]all?)|pbjs|pe(r(formance|imeter.?x|mutive|sonaliz[a-z]*)|tametrics)|pi(ano\.io|co\.tools|n(gdom|img|terest)|wik)|po(rpoiseant|strelease)|prebid[a-z]+|pub(\.network|m(atic|ine))|push(bullet|ly|nami)|px-cloud|
 quora|qua(l(aroo|trics)|nt(cast|serve?)\.)|
 radiateb2b|r-login|revcontent|rightmessage|rlcdn|rubicon|
 sa(il.?(horizon|thr[a-z]+)|lesloft\.com)|sc(ene7|(arab|orecard)research)|se(archiq|edtag|gment\.(com|io)|ntry-cdn|rv(edby|ice.?worker)|ssioncam)|shopify|signup|slickstream|smart(asset|look)|snowplow|so(cial|nobi|vrn)|spo(nsor[a-z]*|t.?im)|statcounter|swoop\.com|synacor[a-z]*|
 ta(boola|rgeting)|tempest|ti(qcdn|nypass)|thinglink|toutapp|[a-z]*tr(ack(cmp|er|ing)[a-z]*|iplelift|ust(pilot|x))|twitter|typekit|
-unruly|urbanairship|us(abilla|er.?agent)|
+unruly|urbanairship|usabilla|
 ve(natus|rizon)|viglink|vuukle|vntsm|
 woocommerce|wp.?(emoji|groho|rum)|
 \.yimg|yotpo|
