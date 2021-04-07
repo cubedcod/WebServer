a12eb05d66e35aed514032bac5071d1e331116e8
user <carmen@replic.net>
2021-04-07 22:36:59 +0000

    p

--- Formats/URI.rb
+++ Formats/URI.rb
@@ -109,14 +109,14 @@ class WebResource < RDF::URI
 
       {class: :toolbox,
        c: [({_: :span, c: env[:status], style: 'font-weight: bold', class: :icon} if env[:status]),                                                      # status code
-           ({_: :a, id: :tabular, class: :icon, c: '↨', href: HTTP.qs(qs.merge({'view' => 'table', 'sort' => 'date'}))} unless env[:view] == 'table'),   # link to tabular view
+           ({_: :a, class: :icon, c: '↨', href: HTTP.qs(qs.merge({'view' => 'table', 'sort' => 'date'}))} unless env[:view] == 'table'),   # link to tabular view
            {_: :a, href: (env[:proxy_href] && !local_node?) ? [env[:base].uri,'#UI'].join : HTTP.qs(qs.merge({'notransform' => nil})), c: '⚗️', id: :UI, class: :icon},# link to origin UX
-           ({_: :a, href: HTTP.qs(qs.merge({'download' => 'audio'})), c: '&darr;', id: :download, class: :icon} if host.match?(/(^|\.)(bandcamp|(mix|sound)cloud|youtube).com/)), # download link
+           ({_: :a, href: HTTP.qs(qs.merge({'download' => 'audio'})), c: '&darr;', class: :icon} if host.match?(/(^|\.)(bandcamp|(mix|sound)cloud|youtube).com/)), # download link
            env[:feeds].map{|feed|                                                                                                                                                 # feed links
-             {_: :a, href: feed.R.href, title: feed.path, class: :icon, c: FeedIcon, id: 'feed' + Digest::SHA2.hexdigest(feed.to_s)}.update(feed.path.match?(/^\/feed\/?$/) ? {style: 'border: .08em solid orange; background-color: orange'} : {})}, "\n",
-           {_: :a, href: env[:base].join('/').R(env).href, id: :host, c: icon ? {_: :img, src: icon, style: DarkLogo.member?(host) ? 'background-color: #fff' : ''} : '🏠'},      # link to path root
+             {_: :a, href: feed.R.href, title: feed.path, class: :icon, c: FeedIcon}.update(feed.path.match?(/^\/feed\/?$/) ? {style: 'border: .08em solid orange; background-color: orange'} : {})}, "\n",
+           {_: :a, href: env[:base].join('/').R(env).href, c: icon ? {_: :img, src: icon, style: DarkLogo.member?(host) ? 'background-color: #fff' : ''} : '🏠'},      # link to path root
            {class: :path, c: env[:base].parts.map{|p| bc += '/' + p                                                                                                               # path breadcrumbs
-              {_: :a, class: :breadcrumb, href: env[:base].join(bc).R(env).href, c: [{_: :span, c: '/'}, (CGI.escapeHTML Rack::Utils.unescape p)], id: 'r' + Digest::SHA2.hexdigest(rand.to_s)}}},
+              {_: :a, class: :breadcrumb, href: env[:base].join(bc).R(env).href, c: [{_: :span, c: '/'}, (CGI.escapeHTML Rack::Utils.unescape p)]}}},
            (if SearchableHosts.member? host
             search_arg = %w(f find q search_query).find{|k|qs.has_key? k} || ([nil, '/'].member?(path) ? 'find' : 'q') # query argument
             qs[search_arg] ||= ''                                                                                      # initial query value
