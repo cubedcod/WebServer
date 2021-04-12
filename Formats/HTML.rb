a9e2833fe70658dc4390e29e72eec834574a01b6
user <carmen@replic.net>
2021-04-12 22:21:56 +0000

    p

--- Formats/HTML.rb
+++ Formats/HTML.rb
@@ -60,11 +60,11 @@ module Webize
       doc.to_html                                    # serialize clean(er) doc
     end
 
-    # format HTML to local preferences
+    # format HTML per local preference
     def self.format html, base
       html = Nokogiri::HTML.fragment html if html.class == String
 
-      # drop upstream formatting and embeds
+      # drop origin formatting and embeds
       html.css('iframe, script, style, a[href^="javascript"], link[rel="stylesheet"], link[type="text/javascript"], link[as="script"]').remove unless [nil,'localhost'].member? base.host
 
       # <img> mapping
@@ -91,41 +91,42 @@ module Webize
             e['src'] = src.uri
           end
         end
-        srcset e, base if e['srcset']                                # resolve @srcset
-        if e['href']                                                 # href attribute
-          ref = (base.join e['href']).R                              # resolve @href
-          ref.query = nil if ref.query&.match?(/utm[^a-z]/)          # deutmize query (tracking gunk)
-          ref.fragment = nil if ref.fragment&.match?(/utm[^a-z]/)    # deutmize fragment
+        srcset e, base if e['srcset']                             # resolve @srcset
+        if e['href']                                              # href attribute
+          ref = (base.join e['href']).R                           # resolve @href
+          ref.query = nil if ref.query&.match?(/utm[^a-z]/)       # deutmize query (tracker gunk)
+          ref.fragment = nil if ref.fragment&.match?(/utm[^a-z]/) # deutmize fragment
           offsite = ref.host != base.host
           e.add_child " <span class='uri'>#{CGI.escapeHTML (offsite ? ref.uri.sub(/^https?:..(www.)?/,'') : (ref.path || '/'))[0..127]}</span> " # show URI in HTML
           css = [:uri]
-          css.push :path unless offsite                              # style as local or global reference
-          css.push :blocked if ref.deny?                             # style as blocked resource
-          e['href'] = ref.href                                       # update href to resolved location
-          e['class'] = css.join ' '                                  # add CSS style
-        elsif e['id']                                                # id attribute w/o href
-          e.set_attribute 'class', 'identified'                      # style as identified node
+          css.push :path unless offsite                           # style as local or global reference
+          css.push :blocked if ref.deny?                          # style as blocked resource
+          e['href'] = ref.href                                    # update href to resolved location
+          e['class'] = css.join ' '                               # add CSS style
+        elsif e['id']                                             # id attribute w/o href
+          e.set_attribute 'class', 'identified'                   # style as identified node
           e.add_child " <a class='idlink' href='##{e['id']}'>##{CGI.escapeHTML e['id'] unless e.name == 'p'}</span> " # add href to node
         end}
 
-      html.to_html                                                   # serialize
+      html.to_html                                                # serialize
     end
 
-    # resolve proxy hrefs in HTML content
+    # rebase hrefs in HTML document
     def self.proxy_hrefs html, env, full=false
-      return '' if !html || html.empty?
-      html = Nokogiri::HTML.send (full ? :parse : :fragment), (html.class==Array ? html.join : html) # parse
+      return '' if !html || html.empty?                           # parse
+      html = Nokogiri::HTML.send (full ? :parse : :fragment), (html.class==Array ? html.join : html)
 
-      html.css('[src]').map{|i|                                            # @src
-        i['src'] = i['src'].R(env).proxy_href}
+      html.css('[src]').map{|i|                                   # @src
+        i['src'] = env[:base].join(i['src']).R(env).proxy_href}
 
-      html.css('[srcset]').map{|i|                                         # @srcset
+      html.css('[srcset]').map{|i|                                # @srcset
         srcset = i['srcset'].scan(SrcSetRegex).map{|ref, size| [ref.R(env).proxy_href, size].join ' '}.join(', ')
         i['srcset'] = srcset unless srcset.empty?}
 
-      html.css('[href]').map{|a|a['href'] = a['href'].R(env).proxy_href}  # @href
+      html.css('[href]').map{|a|
+        a['href'] = env[:base].join(a['href']).R(env).proxy_href} # @href
 
-      html.to_html                                                         # serialize
+      html.to_html                                                # serialize
     end
 
     class Format < RDF::Format
