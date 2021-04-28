18f1c8aeb34f99ee872115fe6911f4b055e86dad
user <carmen@replic.net>
2021-04-28 22:39:11 +0000

    p

--- Formats/Message.rb
+++ Formats/Message.rb
@@ -1,8 +1,79 @@
 # coding: utf-8
+module Webize
+  module Plaintext
+
+    class Reader
+
+      # IRC log -> RDF
+      def chat_triples
+
+        # irssi:
+        #  /set autolog on
+        #  /set autolog_path ~/web/%Y/%m/%d/%H/$tag.$0.irc
+        # weechat:
+        #  /set logger.mask.irc "%Y/%m/%d/%H/$server.$channel.irc"
+
+        type = (SIOC + 'InstantMessage').R
+        parts = @base.parts
+        dirname = File.dirname @base.path
+        network, channame = @base.basename.split '.'
+        channame = Rack::Utils.unescape_path(channame).gsub('#','')
+        chan = ('#' + channame).R
+        day = parts[0..2].join('-') + 'T'
+        hourslug = parts[0..3].join
+        linksubject = [nil, parts[0..2]].join('/') + '/#IRClinks'
+        lines = 0
+        ts = {}
+        @doc.lines.grep(/^[^-]/).map{|msg|
+          tokens = msg.split /\s+/
+          time = tokens.shift
+          if ['*','-!-'].member? tokens[0] # actions, joins, parts
+            nick = tokens[1]
+            msg = tokens[2..-1].join ' '
+            msg = '/me ' + msg if tokens[0] == '*'
+          elsif tokens[0].match? /^-.*:.*-$/ # notices
+            nick = tokens[0][1..tokens[0].index(':')-1]
+            msg = tokens[1..-1].join ' '
+          elsif re = msg.match(/<[\s@+*]*([^>]+)>\s?(.*)?/)
+            nick = re[1]
+            msg = re[2]
+          end
+          nick = CGI.escape(nick || 'anonymous')
+          timestamp = day + time
+          subject = '#' + channame + hourslug + (lines += 1).to_s
+          yield subject, Type, type
+          ts[timestamp] ||= 0
+          yield subject, Date, [timestamp, '%02d' % ts[timestamp]].join('.')
+          ts[timestamp] += 1
+          yield subject, To, chan
+          creator = (dirname + '/*irc?q=' + nick + '&sort=date&view=table#' + nick).R
+          yield subject, Creator, creator
+          yield subject, Content, ['<pre>',
+                                   msg.hrefs{|p,o| yield linksubject, p, o},
+                                   '</pre>'].join if msg}
+      end
+
+      def twtxt_triples
+        dirname = File.dirname @base.path
+        @doc.lines.grep(/^[^#]/).map{|line|
+          date, msg = line.split /\t/
+          graph = @base.join (dirname == '/' ? '' : dirname) + '/twtxt.' + date.gsub(/\D/,'.')
+          subject = graph.join '#msg'
+          yield subject, Type, Post.R, graph
+          yield subject, Date, date, graph
+          yield subject, Content, Webize::HTML.format(msg.hrefs, @base), graph if msg
+          yield subject, Creator, (@base.host + dirname).split(/\W/).join('.'), graph
+          yield subject, To, @base, graph
+        }
+      end
+    end
+  end
+end
+
 class WebResource
   module HTML
 
-    # RDF from message-board post
+    # board-post -> RDF
     def Chan doc
       doc.css('.post, .postCell, .post-container').map{|post|
         num = post.css('a.linkSelf, a.post_no, .postNum a')[0]                # post identifier
@@ -74,6 +145,14 @@ class WebResource
         color = env[:colors][name] ||= '#%06x' % (rand 16777216)
         {_: :a, href: uri.href, class: :fromto, style: "background-color: #{color}; color: black", c: name}
       end}
+
+    MarkupGroup[SIOC+'InstantMessage'] = -> msgs, env {
+      msgs.group_by{|p|(p[To] || [''.R])[0]}.map{|to, msgs|
+        msgs.map{|msg| msg.delete To}
+        {class: :container,
+         c: [{class: :head, c: to.R.display_name, _: :a, href: to},
+             {class: :body, c: (HTML.tabular msgs, env)}]}
+      }}
 
     MarkupGroup[Post] = -> posts, env {
       if env[:view] == 'table'
--- index.rb
+++ index.rb
@@ -1,4 +1,4 @@
-{Formats: %w(URI Archive Audio Calendar Code CSS CSV Feed Form HTML Image JSON Mail Markdown Message MIME Org PDF RDF Subtitle Text Chat Video),
+{Formats: %w(URI Archive Audio Calendar Code CSS CSV Feed Form HTML Image JSON Mail Markdown Message MIME Org PDF RDF Subtitle Text Video),
  Protocols: %w(POSIX HTTP),
  config: %w(gunk meta site)}.
   map{|category, components|
