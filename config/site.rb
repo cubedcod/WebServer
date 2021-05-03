dab9ab02fab1c7fd8e100af67a0065b5b39d9943
user <carmen@replic.net>
2021-05-03 04:19:24 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -300,20 +300,22 @@ l.facebook.com l.instagram.com
             body = response.read
             if response.meta['content-type'].index 'json'
               json = ::JSON.parse HTTP.decompress({'Content-Encoding' => response.meta['content-encoding']}, body)
-              user = json['data']['user']['legacy']
-              uid = json['data']['user']['rest_id']
-
-              r.env[:repository] ||= RDF::Repository.new # RDF storage for profile triples
-              r.env[:repository] << RDF::Statement.new(r, Abstract.R, user['description'].hrefs)
-              r.env[:repository] << RDF::Statement.new(r, Date.R, user['created_at'])
-              r.env[:repository] << RDF::Statement.new(r, Title.R, user['name'])
-              r.env[:repository] << RDF::Statement.new(r, (Schema+'location').R, user['location'])
-              %w(profile_banner_url profile_image_url_https).map{|i|
-                if image = user[i]
-                  r.env[:repository] << RDF::Statement.new(r, Image.R, image.R)
-                end}
-
-              ('https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=false&userId=' + uid + '&count=20' + cursor + '&ext=mediaStats%2CcameraMoment').R(r.env).fetch
+              if json['data'].empty?
+                r.notfound
+              else
+                user = json['data']['user']['legacy']
+                uid = json['data']['user']['rest_id']
+                r.env[:repository] ||= RDF::Repository.new # RDF storage for profile triples
+                r.env[:repository] << RDF::Statement.new(r, Abstract.R, user['description'].hrefs)
+                r.env[:repository] << RDF::Statement.new(r, Date.R, user['created_at'])
+                r.env[:repository] << RDF::Statement.new(r, Title.R, user['name'])
+                r.env[:repository] << RDF::Statement.new(r, (Schema+'location').R, user['location'])
+                %w(profile_banner_url profile_image_url_https).map{|i|
+                  if image = user[i]
+                    r.env[:repository] << RDF::Statement.new(r, Image.R, image.R)
+                  end}
+                ('https://api.twitter.com/2/timeline/profile/' + uid + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=false&userId=' + uid + '&count=20' + cursor + '&ext=mediaStats%2CcameraMoment').R(r.env).fetch
+              end
             else
               [200, response.meta, [body]]
             end}
