ddc78ef6bd45a3ccde1a9cf24098d06b9bee8ed3
user <carmen@replic.net>
2021-05-07 01:01:36 +0000

    p

--- config/site.rb
+++ config/site.rb
@@ -53,6 +53,7 @@ class WebResource
       'forum.solidproject.org' => '#7c4dff',
       'i.redd.it' => '#f40',
       'www.instagram.com' => '#fd7',
+      't.co' => 'rgba(29,161,242,1.00)',
       'twitter.com' => 'rgba(29,161,242,1.00)',
       'www.reddit.com' => '#f40',
       'www.youtube.com' => '#f00',
